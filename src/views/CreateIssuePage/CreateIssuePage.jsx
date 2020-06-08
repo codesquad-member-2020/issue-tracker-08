cd923543616277d413e8d8904281e534f5577251
Lee SeEun <sally4405@naver.com>
2020-06-08 18:21:05 +0900

    [#13] feat: 컴포넌트 구조 설계

---
+++ src/views/CreateIssuePage/CreateIssuePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateIssuePage = () => {
+  return <div></div>;
+};
+
+export default CreateIssuePage;
---
+++ src/views/MilestonePage/MilestonePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MilestonePage = () => {
+  return <div></div>;
+};
+
+export default MilestonePage;
