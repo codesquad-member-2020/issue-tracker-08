ded70b0b228c3d0bce6f414a7ca584fe800127ac
Lee SeEun <sally4405@naver.com>
2020-06-08 18:21:05 +0900

    [#13] feat: 컴포넌트 구조 설계

---
+++ FE/src/components/FilterButton/FilterVerticalList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FilterVerticalList = () => {
+  return <div></div>;
+};
+
+export default FilterVerticalList;
---
+++ FE/src/views/CreateMilestonePage/CreateMilestonePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateMilestonePage = () => {
+  return <div></div>;
+};
+
+export default CreateMilestonePage;
