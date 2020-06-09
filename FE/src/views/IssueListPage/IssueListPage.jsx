64b5869e07cc96657e2f89b174a1e6bb27cd87c9
choisohyun <sohyun4283@naver.com>
2020-06-10 00:27:10 +0900

    [#3] feat: 이슈 리스트 페이지 스타일 작성

--- FE/src/views/IssueListPage/IssueListPage.jsx
+++ FE/src/views/IssueListPage/IssueListPage.jsx
@@ -45,26 +45,88 @@ const IssueListPage = () => {
   );
 };
 
-const Header = styled.header``;
+const Header = styled.header`
+  width: 100%;
+  height: 80px;
+  background-color: ${({ theme }) => theme.colors.black};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+`;
 
-const HeaderIcon = styled(SubjectIcon)``;
+const HeaderIcon = styled(SubjectIcon)`
+  color: ${({ theme }) => theme.colors.lightGray};
+  margin-right: 5px;
+`;
 
-const NavBarWrap = styled.nav``;
+const NavBarWrap = styled.nav`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 40px;
+`;
 
-const NavBar = styled.nav``;
+const NavBar = styled.nav`
+  width: 65%;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
-const SearchBarWrapper = styled.div``;
+const SearchBarWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 55%;
+`;
 
-const SearchBar = styled.form``;
+const SearchBar = styled.form`
+  position: relative;
+  height: 100%;
+  width: 100%;
+`;
 
-const SearchInput = styled.input``;
+const SearchInput = styled.input`
+  padding-left: 30px;
+  background-color: ${({ theme }) => theme.colors.frenchGray};
+  color: ${({ theme }) => theme.colors.gray};
+  border-radius: 5px;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  border: 1px solid ${({ theme }) => theme.colors.lightGray};
+  outline: none;
+  height: inherit;
+  width: inherit;
+`;
 
-const SearchInputIcon = styled(SearchIcon)``;
+const SearchInputIcon = styled(SearchIcon)`
+  position: absolute;
+  top: 9px;
+  left: 8px;
+  display: block;
+  color: ${({ theme }) => theme.colors.lightGray};
+  text-align: center;
+  pointer-events: none;
+`;
 
-const IssueListWrapper = styled.div``;
+const IssueListWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 100px;
+`;
 
-const IssueList = styled.div``;
+const IssueList = styled.div`
+  border: 1px solid ${({ theme }) => theme.colors.lightGray};
+  border-radius: 5px;
+  width: 65%;
+`;
 
-const IssueHeader = styled.div``;
+const IssueHeader = styled.div`
+  background-color: ${({ theme }) => theme.colors.frenchGray};
+  height: 50px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.lightGray};
+`;
 
 export default IssueListPage;
