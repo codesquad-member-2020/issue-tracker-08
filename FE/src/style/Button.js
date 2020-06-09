edcfbb7d15b7d84a9fcdccab84bf7dccf34b48e9
Lee SeEun <sally4405@naver.com>
2020-06-09 17:41:12 +0900

    [#17] feat: Button 공용 스타일로 만들기 위해 props로 받아 처리하는 코드 작성
    - theme color white 추가

--- FE/src/style/Button.js
+++ FE/src/style/Button.js
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
-const Button = styled.button``;
+const Button = styled.button`
+  background-color: ${({ theme, backgroudColor }) =>
+    theme.colors[backgroudColor] || theme.colors.green};
+  color: ${({ theme, color }) => theme.colors[color] || theme.colors.white};
+  font-size: ${({ theme, fontSize }) => theme.fontSizes[fontSize]};
+  pointer-events: ${({ disabled }) => (disabled ? "none" : "auto")};
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+  opacity: ${({ disabled }) => (disabled ? 0.3 : 1)};
+  border-radius: 10px;
+  border: none;
+  text-align: center;
+`;
 
 export default Button;
