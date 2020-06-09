44bf18e371aa6cf67fe6e7c4e283c4cbeffed382
sally4405 <seeun951129@gmail.com>
2020-06-10 04:09:08 +0900

    [#2] fix: Button 컴포넌트의 속성 중 오타 수정
    - backgroudColor > backgroundColor
    - border-radius 값 줄임 (font-size를 통해서 길이를 정해줘야 할 것 같음)

--- src/style/Button.js
+++ src/style/Button.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  background-color: ${({ theme, backgroudColor }) =>
-    theme.colors[backgroudColor] || theme.colors.green};
+  background-color: ${({ theme, backgroundColor }) =>
+    theme.colors[backgroundColor] || theme.colors.green};
   color: ${({ theme, color }) => theme.colors[color] || theme.colors.white};
   font-size: ${({ theme, fontSize }) => theme.fontSizes[fontSize]};
   pointer-events: ${({ disabled }) => (disabled ? "none" : "auto")};
   cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
   opacity: ${({ disabled }) => (disabled ? 0.3 : 1)};
-  border-radius: 10px;
+  border-radius: 5px;
   border: none;
   text-align: center;
 `;
