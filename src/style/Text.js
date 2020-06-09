a1e4df05f9e4a5b3c9420b090b2ea58e4171834e
sally4405 <seeun951129@gmail.com>
2020-06-10 04:09:53 +0900

    [#2] fix: Text 컴포넌트 color 속성의 초기값 설정
    - color을 지정하지 않을 경우 theme의 black으로 설정

--- src/style/Text.js
+++ src/style/Text.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Text = styled.span`
   display: inline-block;
-  color: ${({ theme, color }) => theme.colors[color]};
+  color: ${({ theme, color }) => theme.colors[color] || theme.colors.black};
   font-size: ${({ theme, fontSize }) => theme.fontSizes[fontSize] || theme.fontSizes.md};
   font-weight: ${({ theme, fontWeight }) =>
     theme.fontWeights[fontWeight] || theme.fontWeights.regular};
