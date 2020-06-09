7a06f976e9af7e6449ac5a296c8330ab456b5885
sally4405 <seeun951129@gmail.com>
2020-06-10 04:12:22 +0900

    [#2] feat: 재사용 personnal input box 컴포넌트 스타일 작성
    - title을 props로 받음

--- FE/src/components/InputBox/PersonalInputBox.jsx
+++ FE/src/components/InputBox/PersonalInputBox.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+import Text from "@Style/Text";
 
-const PersonalInputBox = () => {
-  return <div></div>;
+const PersonalInputBox = ({ title }) => {
+  return (
+    <Wrap>
+      <Text children={title} fontWeight="bold" />
+      <InputBox type="text" />
+    </Wrap>
+  );
 };
 
 export default PersonalInputBox;
+
+const Wrap = styled.div`
+  text-align: initial;
+  display: flex;
+  flex-direction: column;
+`;
+
+const InputBox = styled.input`
+  border: none;
+  margin: 5px 0 20px 0;
+  height: 28px;
+  /* box-shadow: 0 0 2px 0 ${({ theme }) => theme.colors.black}; */
+  border-radius: 5px;
+  border: 1px solid ${({ theme }) => theme.colors.lightGray};
+`;
