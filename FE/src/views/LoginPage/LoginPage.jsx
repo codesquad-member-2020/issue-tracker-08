ba89724efa241ae6f46a18b7656ced39ea9d1ed2
sally4405 <seeun951129@gmail.com>
2020-06-10 04:14:50 +0900

    [#2] feat: 로그인 페이지 구조 및 스타일 작성
    - 회원 가입 버튼을 누르면 display를 none, block으로 toggle

--- FE/src/views/LoginPage/LoginPage.jsx
+++ FE/src/views/LoginPage/LoginPage.jsx
@@ -1,7 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import Text from "@Style/Text";
+import Button from "@Style/Button";
+import PersonalInputBox from "@InputBox/PersonalInputBox";
+
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 const LoginPage = () => {
-  return <div></div>;
+  const [isSignupOpen, setIsSignupOpen] = useState(false);
+
+  return (
+    <div>
+      <LoginPageWrap>
+        <Text fontSize="xl" color="black" fontWeight="extraBold">
+          이슈 트래커
+        </Text>
+        <BoxWrap>
+          <LoginWrap isSignupOpen={isSignupOpen}>
+            <PersonalInputBox title="아이디" />
+            <PersonalInputBox title="비밀번호" />
+            <ButtonWrap>
+              <Button backgroundColor="blue" style={loginButtonStyle}>
+                로그인
+              </Button>
+              <Button
+                backgroundColor="blue"
+                style={loginButtonStyle}
+                onClick={() => setIsSignupOpen(!isSignupOpen)}
+              >
+                회원가입
+              </Button>
+            </ButtonWrap>
+            <Button backgroundColor="gray" style={githubButtonStyle}>
+              Sign in with Github
+              {/* <GithubLogoImg
+                src="https://pngimg.com/uploads/github/github_PNG40.png"
+                alt="Github logo"
+              /> */}
+              <GitHubIcon style={githubLogoStyle} />
+            </Button>
+          </LoginWrap>
+          <SignupWrap isSignupOpen={isSignupOpen}>
+            <PersonalInputBox title="아이디" />
+            <PersonalInputBox title="비밀번호" />
+            <PersonalInputBox title="비밀번호 확인" />
+            <PersonalInputBox title="이름" />
+            <Button
+              backgroundColor="blue"
+              style={loginButtonStyle}
+              onClick={() => setIsSignupOpen(!isSignupOpen)}
+            >
+              회원가입
+            </Button>
+          </SignupWrap>
+        </BoxWrap>
+      </LoginPageWrap>
+    </div>
+  );
 };
 
 export default LoginPage;
+
+const LoginPageWrap = styled.div`
+  padding: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+const BoxWrap = styled.div`
+  background-color: ${({ theme }) => theme.colors.frenchGray};
+  margin: 30px;
+  width: 400px;
+  padding: 20px;
+  /* box-shadow: 0 0 2px 0 ${({ theme }) => theme.colors.black}; */
+  border: 1px solid ${({ theme }) => theme.colors.lightGray};
+`;
+
+const LoginWrap = styled.div`
+  display: ${(props) => (props.isSignupOpen ? "none" : "block")};
+`;
+
+const SignupWrap = styled.div`
+  display: ${(props) => (props.isSignupOpen ? "block" : "none")};
+`;
+
+const ButtonWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const loginButtonStyle = {
+  width: "49%",
+  height: "28px",
+  textAlign: "center",
+};
+
+const githubButtonStyle = {
+  width: "100%",
+  height: "28px",
+  margin: "5px 0",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const githubLogoStyle = {
+  height: "20px",
+  margin: "5px",
+};
