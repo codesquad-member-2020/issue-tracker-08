eab174806e52cdff13e78f7be6e5c6494e91aa3a
choisohyun <sohyun4283@naver.com>
2020-06-09 17:02:20 +0900

    [#17] feat: styled component 테마 사용을 위한 색, fontSize 속성 추가
    - 이번 프로젝트에서 사용할 색과 폰트 사이즈를 미리 테마로 정해 둠

---
+++ src/style/theme.js
@@ -0,0 +1,24 @@
+const color = {
+  blue: "#00adb5",
+  skyblue: "#cbf1f5",
+  green: "#00b8a9",
+  gray: "#393e46",
+  lightGray: "#eee",
+  black: "#222831",
+  red: "#fc5185",
+};
+
+const fontSizes = {
+  sm: "12px",
+  md: "16px",
+  lg: "20px",
+  xl: "24px",
+  xxl: "28px",
+};
+
+const theme = {
+  color,
+  fontSizes,
+};
+
+export default theme;
