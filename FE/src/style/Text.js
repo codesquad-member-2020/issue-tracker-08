55e085022955bae2504d7212281bb52b1df44faf
sally4405 <seeun951129@gmail.com>
2020-06-09 20:58:19 +0900

    hotfix: 테마에 font weight 속성 추가
    - Text 에도 font weight 속성 지정 가능하게 추가

--- FE/src/style/Text.js
+++ FE/src/style/Text.js
@@ -4,6 +4,8 @@ const Text = styled.span`
   display: inline-block;
   color: ${({ theme, color }) => theme.colors[color]};
   font-size: ${({ theme, fontSize }) => theme.fontSizes[fontSize] || theme.fontSizes.md};
+  font-weight: ${({ theme, fontWeight }) =>
+    theme.fontWeights[fontWeight] || theme.fontWeights.regular};
 `;
 
 export default Text;
