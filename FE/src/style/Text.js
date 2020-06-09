2d9d1c4bc297e63136c6731eeebf39ba745d801b
choisohyun <sohyun4283@naver.com>
2020-06-09 20:23:49 +0900

    hotfix: 테마의 이름 단/복수 맞게 수정
    - theme에는 복수, 사용할 때는 단수가 되도록 수정함

--- FE/src/style/Text.js
+++ FE/src/style/Text.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const Text = styled.span`
   display: inline-block;
-  color: ${({ theme, color }) => theme.color[color]};
-  font-size: ${({ theme, fontSizes }) => theme.fontSizes[fontSizes] || theme.fontSizes.md};
+  color: ${({ theme, color }) => theme.colors[color]};
+  font-size: ${({ theme, fontSize }) => theme.fontSizes[fontSize] || theme.fontSizes.md};
 `;
 
 export default Text;
