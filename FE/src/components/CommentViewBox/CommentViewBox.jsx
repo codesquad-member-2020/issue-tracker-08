aa0326662a3acb8794dad51be9c344d32ae9cfd5
choisohyun <sohyun4283@naver.com>
2020-06-11 18:41:41 +0900

    [#5] feat: 코멘트 컴포넌트 스타일 적용
    - 스타일은 깃허브 이슈 상세 화면 참고해 적용함

--- FE/src/components/CommentViewBox/CommentViewBox.jsx
+++ FE/src/components/CommentViewBox/CommentViewBox.jsx
@@ -29,27 +29,94 @@ const CommentViewBox = ({ owner }) => {
 };
 
 const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  padding: 16px 0;
+  margin: 0 16px;
+  &::before {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    display: block;
+    width: 2px;
+    content: "";
+    background-color: ${({ theme }) => theme.colors.gray2};
+  }
 `;
 
 const Avatar = styled.img`
+  position: absolute;
+  left: -72px;
+  z-index: 1;
+  width: 40px;
+  height: 40px;
+  border-radius: 3px;
 `;
 
 const CommentGroup = styled.div`
+  width: 100%;
+  z-index: 2;
+  background-color: ${({ theme }) => theme.colors.white};
+  color: ${({ theme }) => theme.colors.gray4};
+  border: 1px solid ${({ theme }) => theme.colors.gray2};
+  border-color: ${({ theme }) => theme.colors.skyblue};
+  flex: auto;
+  border-radius: 3px;
+  margin-left: -16px;
+  &::before {
+    color: ${({ theme }) => theme.colors.skyblue};
+    position: absolute;
+    top: 28px;
+    left: -28px;
+    content: "◀︎";
+  }
 `;
 
 const CommentHeader = styled.div`
+  width: 100%;
+  height: 40px;
+  background-color: ${({ theme, owner }) => (owner ? theme.colors.babyblue : theme.colors.gray2)};
+  border-bottom-color: #c0d3eb;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-right: 16px;
+  padding-left: 16px;
+  color: #586069;
+  flex-direction: row;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray3};
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
 `;
 
 const CommentContent = styled.div`
+  width: 100%;
+  height: 200px;
+  padding: 15px;
+  overflow: visible;
 `;
 
 const CommentText = styled.div`
+  display: flex;
+  ${Text} {
+    margin-right: 10px;
+  }
 `;
 
 const CommentAction = styled.div`
+  ${Text} {
+    margin-left: 10px;
+  }
 `;
 
 const Badge = styled.span`
+  border: 1px solid ${({ theme }) => theme.colors.gray3};
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+  color: ${({ theme }) => theme.colors.gray4};
+  padding: 3px;
+  border-radius: 3px;
 `;
 
 export default CommentViewBox;
