31d57493b5a8088b886683eee29406861b27b59c
Jay <beginin15@gmail.com>
2020-06-11 22:33:05 +0900

    [#29] refactor: Assignee 제거, User로 대체
    - Assignee, Author 모두 User로 표현

--- BE/src/main/java/com/codesquad/issuetracker/User.java
+++ BE/src/main/java/com/codesquad/issuetracker/User.java
@@ -1,20 +1,24 @@
 package com.codesquad.issuetracker;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 @JsonIgnoreProperties({"id", "email", "password"})
 public class User {
 
-
     private Long id;
 
+    @NonNull
     private String nickname;
 
     private String password;
 
     private String email;
+
+    @NonNull
+    private String avatarUrl;
 }
