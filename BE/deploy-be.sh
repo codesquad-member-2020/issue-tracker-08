f7764ab5c9f5f32a4a20a2b3239bee04eb6b2059
Kim HyunSik <hsik0225@gmail.com>
2020-06-11 21:18:28 +0900

    [#37] feat: appsepc.yml 파일 및 deploy shell script 생성

---
+++ BE/deploy-be.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+REPOSITORY=/home/ubuntu/issue-tracker
+cd $REPOSITORY
+
+APP_NAME=airbnb
+JAR_NAME=$(basename $REPOSITORY/build/libs/*.jar)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할것 없음."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -9 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> $JAR_PATH 배포"
+nohup java -jar -Dspring.profiles.active=production $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
