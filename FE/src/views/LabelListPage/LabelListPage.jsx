842420fff555d869e6d4468c5c06760c8fcbee7c
sally4405 <seeun951129@gmail.com>
2020-06-12 06:43:26 +0900

    [#4] feat: 라벨 목록 화면 작성
    - 라벨 edit, delete 기능 추가 필요
    - 라벨 생성시 cancel 기능 추가 필요

--- FE/src/views/LabelListPage/LabelListPage.jsx
+++ FE/src/views/LabelListPage/LabelListPage.jsx
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-import Header from "@Header/Header";
+import Button from "@Style/Button";
+
+import Label from "@LabelListPage/Label/Label";
+import LabelListHeader from "@LabelListPage/LabelListHeader/LabelListHeader";
+import NavigationButton from "@NavigationButton/NavigationButton";
 import CreateLabel from "@LabelListPage/CreateLabel/CreateLabel";
+import Header from "@Header/Header";
+import Table from "@Table/Table";
 
-const LabelListPage = () => {
+const LabelListPage = (props) => {
+  const [isOpenNewLabel, setIsOpenNewLabel] = useState(false);
+
+  const labelList = (
+    <>
+      <Label></Label>
+      <Label></Label>
+    </>
+  );
+
   return (
     <>
-      <Header />
-      <CreateLabel />
+      <Header history={props.history} />
+      <NavBarWrap>
+        <NavBar>
+          <NavigationButton history={props.history} isLabel />
+          <Button onClick={() => setIsOpenNewLabel(!isOpenNewLabel)}>New Label</Button>
+        </NavBar>
+      </NavBarWrap>
+      {isOpenNewLabel && <CreateLabel />}
+      <div style={{ height: "20px" }}></div>
+      <Table tableHeader={<LabelListHeader count={2} />} tableList={labelList} />
     </>
   );
 };
+
+const NavBarWrap = styled.nav`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 40px;
+`;
+
+const NavBar = styled.nav`
+  width: 65%;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export default LabelListPage;
