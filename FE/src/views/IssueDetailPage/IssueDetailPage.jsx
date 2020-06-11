5009fa5e4bc8ea05ef755e7de1ce33e59733e0f3
choisohyun <sohyun4283@naver.com>
2020-06-11 18:33:36 +0900

    [#5] feat:코멘트 재사용 컴포넌트와 필터리스트로 이슈 상세 페이지 UI 구성

--- FE/src/views/IssueDetailPage/IssueDetailPage.jsx
+++ FE/src/views/IssueDetailPage/IssueDetailPage.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import styled from "styled-components";
+
+import FilterVerticalList from "@FilterButton/FilterVerticalList";
+import Header from "@Header/Header";
+import CommentViewBox from "@CommentViewBox/CommentViewBox";
 
 const IssueDetailPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <ContentWrapper>
+        <Content>
+          <CommentViewBoxWrapper>
+            <CommentViewBox owner />
+            <CommentViewBox />
+            <CommentViewBox />
+          </CommentViewBoxWrapper>
+          <FilterVerticalList />
+        </Content>
+      </ContentWrapper>
+    </>
+  );
 };
+
+const ContentWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  width: 65%;
+  display: flex;
+  justify-content: space-between;
+  border-top: 1px solid ${({ theme }) => theme.colors.gray2};
+  margin-top: 16px;
+  padding-left: 72px;
+`;
+
+const CommentViewBoxWrapper = styled.div`
+  width: 75%;
+`;
 
 export default IssueDetailPage;
