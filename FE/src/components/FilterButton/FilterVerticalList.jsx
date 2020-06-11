c40d968d1ef39d4dfeba443faa6ccd6cf1c17b8b
choisohyun <sohyun4283@naver.com>
2020-06-11 09:15:18 +0900

    [#35] feat: FilterButton 컴포넌트 바탕으로 FilterVerticalList 컴포넌트 생성

--- FE/src/components/FilterButton/FilterVerticalList.jsx
+++ FE/src/components/FilterButton/FilterVerticalList.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import styled from "styled-components";
+
+import FilterButton from "@FilterButton/FilterButton";
 
 const FilterVerticalList = () => {
-  return <div></div>;
+  return (
+    <>
+      <Wrapper>
+        <FilterButton title="Assignees" data={assignees}></FilterButton>
+        <FilterButton title="Labels" data={labels}></FilterButton>
+        <FilterButton title="Milestone" data={labels}></FilterButton>
+      </Wrapper>
+    </>
+  );
 };
 
+const Wrapper = styled.div`
+  width: 221px;
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  justify-content: space-around;
+  margin: 10px;
+`;
+
 export default FilterVerticalList;
+
+const assignees = [
+  {
+    name: "jay",
+    color: "#fff",
+    img: "https://avatars1.githubusercontent.com/u/30427711?s=60&v=4",
+    description: "Good for newcomers",
+  },
+  {
+    name: "sally",
+    color: "#fff",
+    img: "https://avatars1.githubusercontent.com/u/30427711?s=60&v=4",
+    description: "Extra attention is needed",
+  },
+  {
+    name: "ever",
+    color: "#fff",
+    img: "https://avatars1.githubusercontent.com/u/30427711?s=60&v=4",
+    description: "",
+  },
+];
+
+const labels = [
+  {
+    name: "good first issue",
+    color: "#7057ff",
+    description: "Good for newcomers",
+  },
+  {
+    name: "help wanted",
+    color: "#008672",
+    description: "Extra attention is needed",
+  },
+  {
+    name: "priority: critical",
+    color: "#b60205",
+    description: "",
+  },
+];
