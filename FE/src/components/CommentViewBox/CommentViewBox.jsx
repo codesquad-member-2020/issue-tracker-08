0c0899b39bc612b4cb3d96fca5b7724f2bab2b4d
choisohyun <sohyun4283@naver.com>
2020-06-11 18:38:39 +0900

    [#5] feat: 코멘트 재사용 컴포넌트 작성
    - 사진, 문구는 임시로 하드코딩함
    - owner라는 boolean의 props를 받아서 뱃지와 헤더 색상을 다르게 표현하도록 함

--- FE/src/components/CommentViewBox/CommentViewBox.jsx
+++ FE/src/components/CommentViewBox/CommentViewBox.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import styled from "styled-components";
 
-const CommentViewBox = () => {
-  return <div></div>;
+import Text from "@Style/Text";
+const CommentViewBox = ({ owner }) => {
+  return (
+    <>
+      <Wrapper>
+        <Avatar src="https://avatars1.githubusercontent.com/u/30427711?s=88&u=0f6f414055ea0bec267856e35e8902b9f728fe1a&v=4"></Avatar>
+        <CommentGroup>
+          <CommentHeader owner={owner ? true : false}>
+            <CommentText>
+              <Text fontWeight="extraBold">choisohyun</Text>
+              <Text color="gray4">
+              </Text>
+            </CommentText>
+            <CommentAction>
+              {owner && <Badge>Owner</Badge>}
+              <Text color="gray4">Edit</Text>
+              <Text color="gray4">Delete</Text>
+            </CommentAction>
+          </CommentHeader>
+          <CommentContent>
+          </CommentContent>
+        </CommentGroup>
+      </Wrapper>
+    </>
+  );
 };
+
+const Wrapper = styled.div`
+`;
+
+const Avatar = styled.img`
+`;
+
+const CommentGroup = styled.div`
+`;
+
+const CommentHeader = styled.div`
+`;
+
+const CommentContent = styled.div`
+`;
+
+const CommentText = styled.div`
+`;
+
+const CommentAction = styled.div`
+`;
+
+const Badge = styled.span`
+`;
 
 export default CommentViewBox;
