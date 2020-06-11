5f00c706caeb1fe59fcbef1f43119dd94bc63f15
sally4405 <seeun951129@gmail.com>
2020-06-12 06:18:12 +0900

    [#6] fix: Text 재사용 컴포넌트에 line-height 추가

--- FE/src/style/Text.js
+++ FE/src/style/Text.js
@@ -4,8 +4,8 @@ const Text = styled.span`
   display: inline-block;
   color: ${({ theme, color }) => theme.colors[color] || theme.colors.black};
   font-size: ${({ theme, fontSize }) => theme.fontSizes[fontSize] || theme.fontSizes.md};
-  font-weight: ${({ theme, fontWeight }) =>
-    theme.fontWeights[fontWeight] || theme.fontWeights.regular};
+  font-weight: ${({ theme, fontWeight }) => theme.fontWeights[fontWeight] || theme.fontWeights.regular};
+  line-height: ${({ lineHeight }) => lineHeight};
 `;
 
 export default Text;
