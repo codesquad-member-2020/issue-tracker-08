5ac4673af6bdd85f45360b3838caaaf230c359cd
Kim HyunSik <hsik0225@gmail.com>
2020-06-12 17:18:29 +0900

    [#30] feat: yml 파일의 속성들을 담을 클래스 생

---
+++ BE/src/main/java/com/codesquad/issuetracker/user/domain/GithubProperty.java
@@ -0,0 +1,33 @@
+package com.codesquad.issuetracker.user.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("github")
+public class GithubProperty {
+
+    @JsonProperty("code")
+    private String code;
+
+    @JsonProperty("client_id")
+    private String clientId;
+
+    @JsonProperty("client_secret")
+    private String clientSecret;
+
+    @JsonIgnore
+    private String codeUrl;
+
+    @JsonIgnore
+    private String userApiUrl;
+
+    @JsonIgnore
+    private String accessTokenUrl;
+}
