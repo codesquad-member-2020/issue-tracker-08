6088b36bb5266c3da57cff57c4e877a5943f551e
sally4405 <seeun951129@gmail.com>
2020-06-12 18:27:31 +0900

    [#8] feat: 새로운 마일스톤 쓰기 화면에서 사용하는 달력 컴포넌트 작성
    - material ui 의 datepickers 활용
    - 기획서에 맞게 커스터마이징함

---
+++ FE/src/views/CreateMilestonePage/DatePickers.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    margin: "5px 0 20px 0",
+  },
+  inputBase: {
+    backgroundColor: "red",
+    padding: 0,
+  },
+  textField: {
+    width: "50%",
+    backgroundColor: "#fafbfc",
+    border: "1px solid #eee",
+    padding: 10,
+    borderRadius: 5,
+  },
+}));
+
+const DatePickers = ({ defaultValue }) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <form className={classes.container} noValidate>
+        <TextField
+          id="date"
+          type="date"
+          defaultValue={defaultValue}
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          inputProps={{
+            style: { padding: 0, fontSize: "13px" },
+          }}
+          InputProps={{ disableUnderline: true }}
+        />
+      </form>
+    </>
+  );
+};
+
+export default DatePickers;
