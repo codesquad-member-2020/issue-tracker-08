915a9916965283c71bc167e994cb3499a718f61b
sally4405 <seeun951129@gmail.com>
2020-06-15 14:50:36 +0900

    [#52] feat: 이슈 상세 화면에 코멘트 생성 컴포넌트 추가 및 스타일링

--- FE/src/components/InputBox/CommentInputBox/CommentInputBox.jsx
+++ FE/src/components/InputBox/CommentInputBox/CommentInputBox.jsx
@@ -31,10 +31,20 @@ const MarkdownInputBox = ({ isIssue, onPass }) => {
             <RawContent type="text" onChange={onSetRawContent} placeholder="Leave a comment" isRawOpen={isRawOpen} />
             <MarkdownConverted content={rawContent} isRawOpen={isRawOpen} />
           </CommentContent>
-          <ButtonWrap>
-            {isIssue && (
+          <ButtonWrap isIssue={isIssue}>
+            {isIssue ? (
               <Button backgroundColor="white" color="black" borderColor="white" onClick={onPass}>
-                Cancle
+                Cancel
+              </Button>
+            ) : (
+              <Button backgroundColor="white" color="black" style={{ marginRight: "5px" }}>
+                <CloseIssueIcon>
+                  <path
+                    fillRule="evenodd"
+                    d="M1.5 8a6.5 6.5 0 0110.65-5.003.75.75 0 00.959-1.153 8 8 0 102.592 8.33.75.75 0 10-1.444-.407A6.5 6.5 0 011.5 8zM8 12a1 1 0 100-2 1 1 0 000 2zm0-8a.75.75 0 01.75.75v3.5a.75.75 0 11-1.5 0v-3.5A.75.75 0 018 4zm4.78 4.28l3-3a.75.75 0 00-1.06-1.06l-2.47 2.47-.97-.97a.749.749 0 10-1.06 1.06l1.5 1.5a.75.75 0 001.06 0z"
+                  ></path>
+                </CloseIssueIcon>
+                Close issue
               </Button>
             )}
             <Button onClick={onPass}>Submit new issue</Button>
@@ -145,7 +155,15 @@ const RawContent = styled.textarea`
 const ButtonWrap = styled.div`
   display: flex;
   padding: 10px;
-  justify-content: space-between;
+  justify-content: ${(props) => (props.isIssue ? "space-between" : "flex-end")};
+`;
+
+const CloseIssueIcon = styled.svg`
+  width: 16px;
+  height: 16px;
+  fill: ${({ theme }) => theme.colors.red};
+  overflow: visible;
+  margin-right: 5px;
 `;
 
 export default MarkdownInputBox;
