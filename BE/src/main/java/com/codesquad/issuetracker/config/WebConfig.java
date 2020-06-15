b70abed7f31301328ed141c24eae97c0f6a71f31
Kim HyunSik <hsik0225@gmail.com>
2020-06-15 16:06:50 +0900

    [#27] feat: webConfig 생

---
+++ BE/src/main/java/com/codesquad/issuetracker/config/WebConfig.java
@@ -0,0 +1,39 @@
+package com.codesquad.issuetracker.config;
+
+import com.codesquad.issuetracker.user.infrastructure.LoginInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .maxAge(3600)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.OPTIONS.name());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/users/**")
+                .excludePathPatterns("/error")
+                .excludePathPatterns("favicon.ico");
+    }
+}
