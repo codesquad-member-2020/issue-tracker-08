3db6f2ec98bf2efa4020b238708c8046f1a7dcd9
Kim HyunSik <hsik0225@gmail.com>
2020-06-15 20:49:55 +0900

    [#55] feat: IssueBoard 밸류 오브젝 클래스 생성

---
+++ BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueBoard.java
@@ -0,0 +1,16 @@
+package com.codesquad.issuetracker.issue.domain;
+
+import java.util.List;
+
+public class IssueBoard {
+
+    private List<Issue> issues;
+
+    private long numberOfLabels;
+
+    private long numberOfMilestones;
+
+    private long numberOfOpenIssues;
+
+    private long numberOfClosedIssues;
+}
