9aca4694a1287e768a604c638f4583db94909f4c
Kim HyunSik <hsik0225@gmail.com>
2020-06-16 12:43:05 +0900

    [#58] feat: MilestoneRepository 정

---
+++ BE/src/main/java/com/codesquad/issuetracker/milestone/domain/MileStoneRepository.java
@@ -0,0 +1,6 @@
+package com.codesquad.issuetracker.milestone.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MileStoneRepository extends CrudRepository<Milestone, Long> {
+}
