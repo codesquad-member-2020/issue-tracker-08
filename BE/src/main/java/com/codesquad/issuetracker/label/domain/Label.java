d681d402b5f887f413e9d0b6cc1d90ea7141362b
Kim HyunSik <hsik0225@gmail.com>
2020-06-16 11:55:28 +0900

    [#57] feat: 라벨 도메인 객체 생

---
+++ BE/src/main/java/com/codesquad/issuetracker/label/domain/Label.java
@@ -0,0 +1,31 @@
+package com.codesquad.issuetracker.label.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Label {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String color;
+
+    private boolean isFontColorBlack;
+}
