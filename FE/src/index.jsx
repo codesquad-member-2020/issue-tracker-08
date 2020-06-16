9f76dce9c300d283cb62479d39fa42580ae9b852
choisohyun <sohyun4283@naver.com>
2020-06-16 17:17:18 +0900

    [#59] feat: redux thunk store 생성하여 Provider 연결

--- FE/src/index.jsx
+++ FE/src/index.jsx
@@ -1,8 +1,18 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import ReduxThunk from "redux-thunk";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
 
 import App from "@/App";
+import modules from "@/modules";
 
 const rootElement = document.getElementById("root");
+const store = createStore(modules, applyMiddleware(ReduxThunk));
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
