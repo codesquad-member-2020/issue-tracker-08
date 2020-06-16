5776a4c721078cc424ec0d33a980b5f862d4b46e
choisohyun <sohyun4283@naver.com>
2020-06-16 21:25:22 +0900

    [#59] feat: 이슈 상세 화면에 파라미터 추가

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
             <Route path="/" exact component={LoginPage} />
             <Route path="/IssueListPage" exact component={IssueListPage} />
             <Route path="/CreateIssuePage" exact component={CreateIssuePage} />
-            <Route path="/IssueDetailPage" exact component={IssueDetailPage} />
+            <Route path="/IssueDetailPage/:issueId" exact component={IssueDetailPage} />
             <Route path="/LabelListPage" exact component={LabelListPage} />
             <Route path="/MilestonePage" exact component={MilestonePage} />
             <Route path="/CreateMilestonePage/:state" component={CreateMilestonePage} />
