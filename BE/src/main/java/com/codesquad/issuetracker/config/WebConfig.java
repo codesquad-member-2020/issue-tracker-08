afb9fb15c74c4e7228f791fc55894341c677bb11
Kim HyunSik <hsik0225@gmail.com>
2020-06-18 19:02:56 +0900

    [#27] fix: allow Method delete 추가

--- BE/src/main/java/com/codesquad/issuetracker/config/WebConfig.java
+++ BE/src/main/java/com/codesquad/issuetracker/config/WebConfig.java
@@ -27,6 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.POST.name(),
                         HttpMethod.PUT.name(),
                         HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
                         HttpMethod.OPTIONS.name());
     }
 
