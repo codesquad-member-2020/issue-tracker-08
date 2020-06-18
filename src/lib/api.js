195c01216512f85bd1a7b3e3d8b04466af883451
sally4405 <sally4405@naver.com>
2020-06-18 09:35:51 +0900

    [#64] feat: getDetail api 추가

--- src/lib/api.js
+++ src/lib/api.js
@@ -6,3 +6,5 @@ export const getIssue = () => axios.get(`${API_URL.issue}`);
 export const getLabel = () => axios.get(`${API_URL.label}`);
 
 export const getMilestone = () => axios.get(`${API_URL.milestone}`);
+// export const getMilestoneDetail = (milestoneId) => axios.get(`${API_URL.milestone}/${milestoneId}/issues`);
+export const getMilestoneDetail = () => axios.get(`${API_URL.milestone}/1/issues`);
