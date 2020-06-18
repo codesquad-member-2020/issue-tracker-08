fa1f361557e44aa260c09a6969b8f3e10b1bec7f
choisohyun <sohyun4283@naver.com>
2020-06-18 10:59:48 +0900

    [#63] feat: 라벨모듈에 라벨 삭제 액션 추가

--- FE/src/modules/label.js
+++ FE/src/modules/label.js
@@ -1,12 +1,14 @@
 import { handleActions } from "redux-actions";
 import createRequestThunk from "../lib/createRequestThunk";
-import { useMemo } from "react";
 import * as api from "@Lib/api";
 
 const GET_LABEL = "label/GET_LABEL";
+const DELETE_LABEL = "label/DELETE_LABEL";
 const GET_LABEL_SUCCESS = "label/GET_LABEL_SUCCESS";
+const DELETE_LABEL_SUCCESS = "label/DELETE_LABEL_SUCCESS";
 
 export const getLabel = createRequestThunk(GET_LABEL, api.getLabel);
+export const deleteLabel = (name) => createRequestThunk(DELETE_LABEL, api.deleteLabel(name));
 
 const initialState = {
   labels: null,
@@ -17,6 +19,10 @@ const label = handleActions(
     [GET_LABEL_SUCCESS]: (state, action) => ({
       ...state,
       labels: action.payload,
+    }),
+    [DELETE_LABEL_SUCCESS]: (state, action) => ({
+      ...state,
+      labels: state.labels.filter((label) => label !== action.payload),
     }),
   },
   initialState
