489a0bea19c99d30fd693d21b03ec150e732f17a
sally4405 <sally4405@naver.com>
2020-06-18 09:36:41 +0900

    [#64] feat: 마일스톤 모듈에 getDetail 액션과 리듀서 추가

--- FE/src/modules/milestone.js
+++ FE/src/modules/milestone.js
@@ -5,10 +5,15 @@ import * as api from "@Lib/api";
 const GET_MILESTONE = "milestone/GET_MILESTONE";
 const GET_MILESTONE_SUCCESS = "milestone/GET_MILESTONE_SUCCESS";
 
+const GET_MILESTONE_DETAIL = "milestone/GET_MILESTONE_DETAIL";
+const GET_MILESTONE_DETAIL_SUCCESS = "milestone/GET_MILESTONE_DETAIL_SUCCESS";
+
 export const getMilestone = createRequestThunk(GET_MILESTONE, api.getMilestone);
+export const getMilestoneDetail = createRequestThunk(GET_MILESTONE_DETAIL, api.getMilestoneDetail);
 
 const initialState = {
   milestones: null,
+  milestoneDetail: null,
 };
 
 const milestone = handleActions(
@@ -16,6 +21,12 @@ const milestone = handleActions(
     [GET_MILESTONE_SUCCESS]: (state, action) => ({
       ...state,
       milestones: action.payload,
+    }),
+  },
+  {
+    [GET_MILESTONE_DETAIL_SUCCESS]: (state, action) => ({
+      ...state,
+      milestoneDetail: action.payload,
     }),
   },
   initialState
