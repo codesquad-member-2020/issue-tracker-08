6c71293d8739b8f78fcea63409aeb1c3fdc51d9d
Kim HyunSik <hsik0225@gmail.com>
2020-06-18 19:38:31 +0900

    [#58] refactor: milestone Id를 ID참조 형태로 변

--- BE/src/main/java/com/codesquad/issuetracker/milestone/domain/Milestone.java
+++ BE/src/main/java/com/codesquad/issuetracker/milestone/domain/Milestone.java
@@ -1,17 +1,18 @@
 package com.codesquad.issuetracker.milestone.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-public class Milestone {
+@Getter
+@Setter
+public class  Milestone {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @EmbeddedId
+    private MilestoneId id;
 
     private String title;
 
