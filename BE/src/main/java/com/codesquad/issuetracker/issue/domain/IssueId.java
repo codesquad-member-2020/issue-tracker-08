57325fd3e1a7eb98c498dfcae49077eccfd7f082
Kim HyunSik <hsik0225@gmail.com>
2020-06-18 21:59:27 +0900

    [#55] refactor: IssueId 어노테이션 수

--- BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueId.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueId.java
@@ -1,14 +1,21 @@
 package com.codesquad.issuetracker.issue.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 @Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
 public class IssueId implements Serializable {
-
 
     private Long issueId;
 }
