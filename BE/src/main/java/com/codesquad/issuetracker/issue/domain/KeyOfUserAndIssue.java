b4d427c4b5b5bb9e8f438d629e3e212ed5098341
Kim HyunSik <hsik0225@gmail.com>
2020-06-18 21:58:48 +0900

    [#55] refactor: Assignee 복합 키 변

--- BE/src/main/java/com/codesquad/issuetracker/issue/domain/KeyOfUserAndIssue.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/domain/KeyOfUserAndIssue.java
@@ -1,17 +1,24 @@
 package com.codesquad.issuetracker.issue.domain;
 
 import com.codesquad.issuetracker.user.domain.UserId;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.EmbeddedId;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
 public class KeyOfUserAndIssue implements Serializable {
 
-    @EmbeddedId
     private UserId userId;
 
-    @EmbeddedId
     private IssueId issueId;
 }
