a6c088d4c77996a0d6899b4bcb4849106d3ad4b8
Jay <beginin15@gmail.com>
2020-06-20 00:04:34 +0900

    [#58] feat: 가장 최근 마일스톤 조회하는 repo 메소드 구현
    - 가장 최근 마일스톤의 id값을 조회하여 해당 값 + 1을 새로운 마일스톤의 id값으로 이용

--- BE/src/main/java/com/codesquad/issuetracker/milestone/domain/MileStoneRepository.java
+++ BE/src/main/java/com/codesquad/issuetracker/milestone/domain/MileStoneRepository.java
@@ -3,4 +3,6 @@ package com.codesquad.issuetracker.milestone.domain;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MileStoneRepository extends CrudRepository<Milestone, Long> {
+
+    public Milestone findFirstByOrderByIdDesc();
 }
