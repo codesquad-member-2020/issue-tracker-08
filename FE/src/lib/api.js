42b19183e526b1e5d0d5a983b9089d233ea52a7f
Lee SeEun <sally4405@naver.com>
2020-06-19 15:22:03 +0900

    [#64] feat: api에 post, get, put, patch, delete 추가

--- FE/src/lib/api.js
+++ FE/src/lib/api.js
@@ -6,5 +6,8 @@ export const getIssue = () => axios.get(`${API_URL.issue}`);
 export const getLabel = () => axios.get(`${API_URL.label}`);
 
 export const getMilestone = () => axios.get(`${API_URL.milestone}`);
-// export const getMilestoneDetail = (milestoneId) => axios.get(`${API_URL.milestone}/${milestoneId}/issues`);
-export const getMilestoneDetail = () => axios.get(`${API_URL.milestone}/1/issues`);
+export const postMilestone = (params) => axios.post(`${API_URL.milestone}`, params);
+export const getMilestoneDetail = (milestoneId) => axios.get(`${API_URL.milestone}${milestoneId}/issues`);
+export const putMilestone = (milestoneId, params) => axios.put(`${API_URL.milestone}${milestoneId}`, params);
+export const patchMilestone = (milestoneId) => axios.patch(`${API_URL.milestone}${milestoneId}`);
+export const deleteMilestone = (milestoneId) => axios.delete(`${API_URL.milestone}${milestoneId}`);
