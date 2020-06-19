c922c47a54e8581d29b522663e9c4caa9580091d
Jay <beginin15@gmail.com>
2020-06-20 03:51:40 +0900

    [#58] refactor: 마일스톤 목록 조회 서비스 메소드 구현
    - getAllMilestones(): 모든 마일스톤 목록을 조회하는 메소드. 개폐 여부 상관없이 모든 마일스톤을 가져옴
    
    - parseMilestoneToDTO(): 레포지토리로부터 조회한 마일스톤 컬렉션을 DTO 컬렉션으로 맵핑하는 메소드. 해당 메소드는 /milestones/{id}/issue에서 재사용될 가능성이 있음.
    
    - getMetaDataOfMilesstones(): 마일스톤에 할당된 각 open, close 이슈의 개수, 성취율 등을 구하는 메소드

--- BE/src/main/java/com/codesquad/issuetracker/milestone/application/MilestoneService.java
+++ BE/src/main/java/com/codesquad/issuetracker/milestone/application/MilestoneService.java
@@ -1,13 +1,18 @@
 package com.codesquad.issuetracker.milestone.application;
 
-import com.codesquad.issuetracker.milestone.domain.MileStoneRepository;
-import com.codesquad.issuetracker.milestone.domain.Milestone;
-import com.codesquad.issuetracker.milestone.domain.MilestoneId;
+import com.codesquad.issuetracker.issue.domain.Issue;
+import com.codesquad.issuetracker.issue.domain.IssueRepository;
+import com.codesquad.issuetracker.milestone.domain.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Slf4j
 @Service
@@ -16,6 +21,8 @@ public class MilestoneService {
 
     private final MileStoneRepository mileStoneRepository;
 
+    private final IssueRepository issueRepository;
+
     public void createMilestone(Milestone milestone) {
         MilestoneId id = new MilestoneId(getNextIdentity());
         milestone.setId(id);
@@ -23,9 +30,57 @@ public class MilestoneService {
         mileStoneRepository.save(milestone);
     }
 
+    public MilestoneBoard getAllMilestones() {
+        List<Milestone> milestones = StreamSupport.stream(mileStoneRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+
+        long numberOfOpenMilestone = milestones.stream().filter(Milestone::isOpen).count();
+        long numberOfClosedMilestone = milestones.stream().filter(m -> !m.isOpen()).count();
+        List<MilestoneDTO> milestoneDTOS = parseMilestonesToDTO(milestones);
+
+        return new MilestoneBoard(numberOfOpenMilestone, numberOfClosedMilestone, milestoneDTOS);
+    }
+
     private long getNextIdentity() {
         return Optional.ofNullable(mileStoneRepository.findFirstByOrderByIdDesc())
                 .map(milestone -> milestone.getId().getMilestoneId())
                 .orElse(0L) + 1;
+    }
+
+    private List<MilestoneDTO> parseMilestonesToDTO(List<Milestone> milestones) {
+        return milestones.stream().map(m -> {
+
+            Map<String, Long> metaData = getMetaDataOfMilestones(StreamSupport.stream(issueRepository.findAllById(m.getIssues()).spliterator(), false)
+                    .collect(Collectors.toList()));
+
+            // 현재는 List<IssueDTO>를 세팅하지 않지만 /milestones/{id}/issues에서 필요할 듯
+            return MilestoneDTO.builder()
+                    .id(m.getId().getMilestoneId())
+                    .title(m.getTitle())
+                    .description(m.getDescription())
+                    .dueDate(m.getDueDate())
+                    .updatedAt(m.getModifiedAt())
+                    .isOpen(m.isOpen())
+                    .achievementRate(metaData.get("achievementRate"))
+                    .numberOfOpenIssue(metaData.get("numberOfOpenIssue"))
+                    .numberOfClosedIssue(metaData.get("numberOfClosedIssue"))
+                    .build();
+        }).collect(Collectors.toList());
+    }
+
+    private Map<String, Long> getMetaDataOfMilestones(List<Issue> issues) {
+        if (issues.size() == 0) {
+            return new HashMap<String, Long>() {{
+                put("numberOfOpenIssue", 0L);
+                put("numberOfClosedIssue", 0L);
+                put("achievementRate", 0L);
+            }};
+        }
+
+        return new HashMap<String, Long>() {{
+            put("numberOfOpenIssue", issues.stream().filter(Issue::isOpen).count());
+            put("numberOfClosedIssue", issues.stream().filter(i -> !i.isOpen()).count());
+            put("achievementRate", (long)(get("numberOfOpenIssue") * 1.f / issues.size() * 100));
+        }};
     }
 }
