68337ec8d6a3d6e65c62069de3273dd99f6de522
Jay <beginin15@gmail.com>
2020-06-20 03:41:44 +0900

    [#58] refactor: Milestone 엔티티 클래스에 issue 연관 관계 설정

--- BE/src/main/java/com/codesquad/issuetracker/milestone/domain/Milestone.java
+++ BE/src/main/java/com/codesquad/issuetracker/milestone/domain/Milestone.java
@@ -1,23 +1,31 @@
 package com.codesquad.issuetracker.milestone.domain;
 
 import com.codesquad.issuetracker.common.BaseTimeEntity;
+import com.codesquad.issuetracker.issue.domain.IssueId;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class  Milestone extends BaseTimeEntity {
+public class Milestone extends BaseTimeEntity {
 
     @EmbeddedId
     private MilestoneId id;
+
+    @ElementCollection (fetch = FetchType.EAGER)
+    @CollectionTable (
+            name = "issue",
+            joinColumns = @JoinColumn(name = "milestone_id")
+    )
+    Set<IssueId> issues = new HashSet<>();
 
     @Column(name = "title", unique = true)
     private String title;
