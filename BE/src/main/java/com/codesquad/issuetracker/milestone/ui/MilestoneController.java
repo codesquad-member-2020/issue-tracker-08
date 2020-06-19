b08c7355caa365f15314bdadef4be229de468264
Jay <beginin15@gmail.com>
2020-06-20 00:19:16 +0900

    [#58] refactor: 마일스톤 생성 시, 동일한 title 생성 시도 예외 처리

--- BE/src/main/java/com/codesquad/issuetracker/milestone/ui/MilestoneController.java
+++ BE/src/main/java/com/codesquad/issuetracker/milestone/ui/MilestoneController.java
@@ -1,13 +1,17 @@
 package com.codesquad.issuetracker.milestone.ui;
 
+import com.codesquad.issuetracker.common.exception.ErrorMessage;
 import com.codesquad.issuetracker.milestone.application.MilestoneService;
-import com.codesquad.issuetracker.milestone.domain.MileStoneRepository;
 import com.codesquad.issuetracker.milestone.domain.Milestone;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -19,7 +23,11 @@ public class MilestoneController {
 
     @PostMapping("")
     public ResponseEntity<String> createMilestone(@RequestBody Milestone milestone) {
-        milestoneService.createMilestone(milestone);
+        try {
+            milestoneService.createMilestone(milestone);
+        } catch (DataIntegrityViolationException e) {
+            return new ResponseEntity<>(ErrorMessage.MILESTONE_TITLE_DUPLICATED.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+        }
         return new ResponseEntity<>("마일스톤 생성 성공", HttpStatus.CREATED);
     }
 }
