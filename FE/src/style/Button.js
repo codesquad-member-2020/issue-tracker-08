d095592dfcbc8cbc442f6aae5778752a2ba1e926
choisohyun <sohyun4283@naver.com>
2020-06-19 15:10:15 +0900

    [#63] fix: 버튼 누를 때 submit되어 reload되지 않도록 type 지정

--- FE/src/style/Button.js
+++ FE/src/style/Button.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const Button = styled.button`
+const Button = styled.button.attrs({ type: "button" })`
   background-color: ${({ theme, backgroundColor }) => theme.colors[backgroundColor] || theme.colors.green};
   color: ${({ theme, color }) => theme.colors[color] || theme.colors.white};
   font-size: ${({ theme, fontSize }) => theme.fontSizes[fontSize] || theme.fontSizes.md};
