3c1932cae99570e84b70b3cc42bc558265a40025
choisohyun <sohyun4283@naver.com>
2020-06-19 15:09:35 +0900

    [#63] feat: 라벨 생성 POST 요청 액션 추가

--- FE/src/modules/label.js
+++ FE/src/modules/label.js
@@ -3,11 +3,17 @@ import createRequestThunk from "../lib/createRequestThunk";
 import * as api from "@Lib/api";
 
 const GET_LABEL = "label/GET_LABEL";
-const DELETE_LABEL = "label/DELETE_LABEL";
 const GET_LABEL_SUCCESS = "label/GET_LABEL_SUCCESS";
+
+const POST_LABEL = "label/POST_LABEL";
+const POST_LABEL_SUCCESS = "label/POST_LABEL_SUCCESS";
+
+const DELETE_LABEL = "label/DELETE_LABEL";
 const DELETE_LABEL_SUCCESS = "label/DELETE_LABEL_SUCCESS";
+const DELETE_LABEL_FAILURE = "label/DELETE_LABEL_FAILURE";
 
 export const getLabel = createRequestThunk(GET_LABEL, api.getLabel);
+export const createLabel = (params) => createRequestThunk(POST_LABEL, api.createLabel(params));
 export const deleteLabel = (name) => createRequestThunk(DELETE_LABEL, api.deleteLabel(name));
 
 const initialState = {
@@ -20,9 +26,17 @@ const label = handleActions(
       ...state,
       labels: action.payload,
     }),
+    [POST_LABEL_SUCCESS]: (state, action) => ({
+      ...state,
+      labels: action.payload,
+    }),
     [DELETE_LABEL_SUCCESS]: (state, action) => ({
       ...state,
       labels: state.labels.filter((label) => label !== action.payload),
+    }),
+    [DELETE_LABEL_FAILURE]: (state, action) => ({
+      ...state,
+      labels: action.payload,
     }),
   },
   initialState
