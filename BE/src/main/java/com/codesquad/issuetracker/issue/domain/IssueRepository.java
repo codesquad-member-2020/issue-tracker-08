a5f1730f3eb96aa8738542fc8428882ddd1f0d61
Kim HyunSik <hsik0225@gmail.com>
2020-06-19 09:11:10 +0900

    [#55] feat: IssueRepository에 SELECT count(*) from Issue 메소드 구

--- BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueRepository.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueRepository.java
@@ -3,4 +3,6 @@ package com.codesquad.issuetracker.issue.domain;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IssueRepository extends CrudRepository<Issue, Long> {
+
+    long count();
 }
