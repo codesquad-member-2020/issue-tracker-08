1cca7746972b277ac8e9e7deb96aa321153c7997
Kim HyunSik <hsik0225@gmail.com>
2020-06-20 22:52:09 +0900

    [#70] feat: 유저 API Controller 스켈레톤 코드 작성

--- BE/src/main/java/com/codesquad/issuetracker/user/ui/UserController.java
+++ BE/src/main/java/com/codesquad/issuetracker/user/ui/UserController.java
@@ -1,22 +1,12 @@
 package com.codesquad.issuetracker.user.ui;
 
 import com.codesquad.issuetracker.user.application.LoginService;
-import com.codesquad.issuetracker.user.domain.GithubProperty;
-import com.codesquad.issuetracker.user.domain.GithubToken;
-import com.codesquad.issuetracker.user.domain.User;
-import com.codesquad.issuetracker.utils.GithubApiUtils;
-import com.codesquad.issuetracker.utils.JwtUtils;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -26,6 +16,16 @@ import java.io.IOException;
 public class UserController {
 
     private final LoginService loginService;
+
+    @GetMapping("")
+    public Object userList() {
+        return null;
+    }
+
+    @PostMapping("")
+    public Object createUser() {
+        return null;
+    }
 
     @GetMapping("/oauth/code")
     public ResponseEntity<Void> getCode() {
