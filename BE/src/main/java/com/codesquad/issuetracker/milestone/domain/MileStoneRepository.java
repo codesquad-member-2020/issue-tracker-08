eb06f8dc850fcbd23acd584158b6a184dcf6b69c
Jay <beginin15@gmail.com>
2020-06-20 18:19:23 +0900

    [#58] feat: 마일스톤 변경 레포지토리 메소드 구현
    - save()로 변경하면 원치 않은 컬럼까지 데이터가 변경되기 때문에 update 쿼리를 수행하는 메소드를 별도로 구현

--- BE/src/main/java/com/codesquad/issuetracker/milestone/domain/MileStoneRepository.java
+++ BE/src/main/java/com/codesquad/issuetracker/milestone/domain/MileStoneRepository.java
@@ -1,8 +1,22 @@
 package com.codesquad.issuetracker.milestone.domain;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface MileStoneRepository extends CrudRepository<Milestone, Long> {
+import javax.transaction.Transactional;
+
+public interface MileStoneRepository extends CrudRepository<Milestone, MilestoneId> {
 
     public Milestone findFirstByOrderByIdDesc();
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Milestone " +
+            "SET  title = :#{#milestone.title}," +
+            "      description = :#{#milestone.description}," +
+            "      due_date = :#{#milestone.dueDate} " +
+            "WHERE milestone_id = :#{#milestone.id.milestoneId}")
+    public void updateMilestone(@Param("milestone") Milestone milestone);
 }
