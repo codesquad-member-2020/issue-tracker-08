29c9ce03a1417ce7d73b77d372210c8abdf38903
Kim HyunSik <hsik0225@gmail.com>
2020-06-20 21:57:58 +0900

    [#69] feat: CommentService 생

---
+++ BE/src/main/java/com/codesquad/issuetracker/comment/application/CommentService.java
@@ -0,0 +1,43 @@
+package com.codesquad.issuetracker.comment.application;
+
+import com.codesquad.issuetracker.comment.domain.Comment;
+import com.codesquad.issuetracker.comment.domain.CommentId;
+import com.codesquad.issuetracker.comment.domain.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public CommentId getNextIdentity() {
+        return Optional.ofNullable(commentRepository.findFirstByOrderByIdDesc())
+                .map(comment -> new CommentId(comment.getId().getCommentId() + 1L))
+                .orElseGet(() -> new CommentId(1L));
+    }
+
+    public Comment addComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    public void update(Comment comment) {
+        commentRepository.save(comment);
+    }
+
+    public void changeStatus(CommentId commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 댓글입니다!"));
+        commentRepository.save(comment);
+    }
+
+    public void delete(CommentId commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 댓글입니다!"));
+        commentRepository.delete(comment);
+    }
+}
