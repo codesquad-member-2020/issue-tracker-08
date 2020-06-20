ac275a3f95e9f924d43bb503ced8b17b77a53bd7
Kim HyunSik <hsik0225@gmail.com>
2020-06-20 21:58:16 +0900

    [#69] refactor: Comment에 issueId 생

--- BE/src/main/java/com/codesquad/issuetracker/comment/domain/Comment.java
+++ BE/src/main/java/com/codesquad/issuetracker/comment/domain/Comment.java
@@ -1,23 +1,31 @@
 package com.codesquad.issuetracker.comment.domain;
 
 import com.codesquad.issuetracker.common.BaseTimeEntity;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.codesquad.issuetracker.issue.domain.IssueId;
+import lombok.*;
 
-import javax.persistence.*;
+import javax.persistence.Embedded;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "comment")
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class Comment extends BaseTimeEntity {
 
     @EmbeddedId
     private CommentId id;
 
+    @Embedded
+    private IssueId issueId;
+
     private String content;
 
-    private boolean isHide;
+    private boolean isOpen;
 }
