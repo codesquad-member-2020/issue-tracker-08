0ddcaab0b3570ffeaa8321e18213546a7491ca3c
Kim HyunSik <hsik0225@gmail.com>
2020-06-20 22:59:18 +0900

    [#70] feat: User 회원가입 기능 구현

--- BE/src/main/java/com/codesquad/issuetracker/user/ui/UserController.java
+++ BE/src/main/java/com/codesquad/issuetracker/user/ui/UserController.java
@@ -1,6 +1,9 @@
 package com.codesquad.issuetracker.user.ui;
 
 import com.codesquad.issuetracker.user.application.LoginService;
+import com.codesquad.issuetracker.user.domain.User;
+import com.codesquad.issuetracker.user.domain.UserId;
+import com.codesquad.issuetracker.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -9,22 +12,36 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserRepository userRepository;
+
     private final LoginService loginService;
 
     @GetMapping("")
-    public Object userList() {
+    public ResponseEntity<String> userList() {
+        userRepository.findAll();
         return null;
     }
 
     @PostMapping("")
-    public Object createUser() {
-        return null;
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        UserId userId = getNextIdentity();
+        User newUser = User.builder()
+                .id(userId)
+                .nickname(user.getNickname())
+                .avatarUrl(user.getAvatarUrl())
+                .email(user.getEmail())
+                .build();
+
+        userRepository.save(newUser);
+
+        return new ResponseEntity<>("유저 생성 성공", HttpStatus.CREATED);
     }
 
     @GetMapping("/oauth/code")
@@ -37,5 +54,11 @@ public class UserController {
     public ResponseEntity<Void> oauth(@RequestParam String code, HttpServletResponse response) throws IOException {
         loginService.login(code, response);
         return new ResponseEntity<>(HttpStatus.FOUND);
+    }
+
+    public UserId getNextIdentity() {
+        return Optional.ofNullable(userRepository.findFirstByOrderByIdDesc())
+                .map(user -> new UserId(user.getId().getUserId() + 1L))
+                .orElseGet(() -> new UserId(1L));
     }
 }
