e3ad1585aa223b1138d2671720ae074fb303307c
Kim HyunSik <hsik0225@gmail.com>
2020-06-20 22:20:16 +0900

    [#69] refactor: 댓글 수정 기능 구

--- BE/src/main/java/com/codesquad/issuetracker/comment/domain/CommentId.java
+++ BE/src/main/java/com/codesquad/issuetracker/comment/domain/CommentId.java
@@ -11,6 +11,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
+@ToString
 public class CommentId implements Serializable {
 
     @Column(name = "comment_id")
