1bf2655822d6b635051cb6613bd053b876a4999e
Kim HyunSik <hsik0225@gmail.com>
2020-06-20 17:57:53 +0900

    [#57] refactor: 라벨 생성 메소드 분

--- BE/src/main/java/com/codesquad/issuetracker/label/application/LabelService.java
+++ BE/src/main/java/com/codesquad/issuetracker/label/application/LabelService.java
@@ -1,8 +1,8 @@
 package com.codesquad.issuetracker.label.application;
 
 import com.codesquad.issuetracker.label.domain.Label;
-import com.codesquad.issuetracker.label.domain.LabelProperty;
 import com.codesquad.issuetracker.label.domain.LabelId;
+import com.codesquad.issuetracker.label.domain.LabelProperty;
 import com.codesquad.issuetracker.label.domain.LabelRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,15 +20,8 @@ public class LabelService {
 
     private final LabelRepository labelRepository;
 
-    public Label saveLabel(LabelProperty label) {
-        LabelId id = new LabelId(getNextIdentity());
-
-        Label newLabel = Label.builder()
-                .id(id)
-                .labelProperty(label)
-                .build();
-
-        return labelRepository.save(newLabel);
+    public Label save(Label label) {
+        return labelRepository.save(label);
     }
 
     public List<LabelProperty> getAllLabels() {
@@ -37,9 +30,9 @@ public class LabelService {
                 .collect(Collectors.toList());
     }
 
-    private long getNextIdentity() {
+    public LabelId getNextIdentity() {
         return Optional.ofNullable(labelRepository.findFirstByOrderByIdDesc())
-                .map(label -> label.getId().getLabelId())
-                .orElse(0L) + 1;
+                .map(label -> new LabelId(label.getId().getLabelId()))
+                .orElseGet(() -> new LabelId(1L));
     }
 }
