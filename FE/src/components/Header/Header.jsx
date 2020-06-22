d19b0e7d9f7075e6cf1943c19c224da41db1338e
choisohyun <sohyun4283@naver.com>
2020-06-22 21:24:13 +0900

    [#83] feat: 쿠키 값을 바탕으로 유저정보 보여지도록 스타일 추가

--- FE/src/components/Header/Header.jsx
+++ FE/src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import SubjectIcon from "@material-ui/icons/Subject";
 import { useHistory } from "react-router-dom";
 
 import Text from "@Style/Text";
+import getCookieValue from "@Lib/getCookieValue";
 
 const Header = () => {
   let history = useHistory();
@@ -27,9 +28,13 @@ const Header = () => {
             ISSUES
           </Text>
         </TitleWrapper>
+        <UserWrapper>
+          <Text color="white">{getCookieValue("nickname")}</Text>
+          <UserImage src={decodeURIComponent(getCookieValue("avatarUrl"))} />
           <Text color="white" isClick onClick={logoutHandler}>
             로그아웃
           </Text>
+        </UserWrapper>
       </Wrapper>
     </>
   );
@@ -43,6 +48,7 @@ const Wrapper = styled.header`
   justify-content: center;
   align-items: center;
   margin-bottom: 40px;
+  position: relative;
 `;
 
 const HeaderIcon = styled(SubjectIcon)`
@@ -53,6 +59,29 @@ const HeaderIcon = styled(SubjectIcon)`
 const TitleWrapper = styled.div`
   display: flex;
   align-items: center;
+`;
+
+const UserWrapper = styled.div`
+  position: absolute;
+  right: 40px;
+  display: flex;
+  align-items: center;
+  height: 40px;
+  line-height: 20px;
+  border-radius: 15px;
+  padding: 0 3px;
+  margin-left: 3px;
+  font-size: 13px;
+  font-weight: bold;
+  box-sizing: border-box;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.18);
+`;
+
+const UserImage = styled.img`
+  height: 30px;
+  width: 30px;
+  border-radius: 5px;
+  margin: 0 15px 0 10px;
 `;
 
 export default Header;
