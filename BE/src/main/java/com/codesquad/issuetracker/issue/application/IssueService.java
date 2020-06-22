e6b322f2b2ce7a32ac8822428676cddd01e7117c
Jay <beginin15@gmail.com>
2020-06-23 00:16:04 +0900

    [#55] fix: feature_be/issues에 dev 머지 과정 충돌 파일 수정

--- BE/src/main/java/com/codesquad/issuetracker/issue/application/IssueService.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/application/IssueService.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
