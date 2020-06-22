a89e3c03949851e320ab196a5df51a092062ad42
Jay <beginin15@gmail.com>
2020-06-22 23:11:10 +0900

    [#70] feat: 로그인 예외 처리 메세지 추가

--- BE/src/main/java/com/codesquad/issuetracker/common/exception/ErrorMessage.java
+++ BE/src/main/java/com/codesquad/issuetracker/common/exception/ErrorMessage.java
@@ -10,7 +10,8 @@ public enum ErrorMessage {
     LABEL_NAME_DUPLICATED("Name has already been taken"),
     MILESTONE_TITLE_DUPLICATED("Title has already been taken"),
     ENTITY_UPDATE_FAILED("Failed to update"),
-    ENTITY_DELETE_FAILED("Failed to delete");
+    ENTITY_DELETE_FAILED("Failed to delete"),
+    UNAUTHORIZED("Need to Login");
 
     private String message;
     
