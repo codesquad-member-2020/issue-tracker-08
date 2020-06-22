15400abfc51d15d02207fb224da3f393b873da8d
Kim HyunSik <hsik0225@gmail.com>
2020-06-22 13:05:07 +0900

    [#70] refactor: getGithubToken 메소드를 서비스가 아닌 클래스의 메소드로 이동

--- BE/src/main/java/com/codesquad/issuetracker/user/application/LoginService.java
+++ BE/src/main/java/com/codesquad/issuetracker/user/application/LoginService.java
@@ -1,16 +1,13 @@
 package com.codesquad.issuetracker.user.application;
 
 import com.codesquad.issuetracker.user.domain.GithubProperty;
-import com.codesquad.issuetracker.user.domain.GithubToken;
 import com.codesquad.issuetracker.user.domain.User;
 import com.codesquad.issuetracker.utils.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,15 +17,10 @@ public class LoginService {
 
     private final GithubProperty githubProperty;
 
-    public void login(User user, HttpServletResponse response) throws IOException {
+    public void login(User user, HttpServletResponse response) {
         String jwtToken = JwtUtils.createToken(user);
         List<Cookie> cookies = createCookiesByUser(user, jwtToken);
         setCookies(cookies, response);
-    }
-
-    public GithubToken getGithubToken(String code) {
-        githubProperty.setCode(code);
-        return new RestTemplate().postForEntity(githubProperty.getAccessTokenUrl(), githubProperty, GithubToken.class).getBody();
     }
 
     private List<Cookie> createCookiesByUser(User user, String jwtToken) {
@@ -40,7 +32,7 @@ public class LoginService {
         return cookies;
     }
 
-    private void setCookies(List<Cookie> cookies, HttpServletResponse response) throws IOException {
+    private void setCookies(List<Cookie> cookies, HttpServletResponse response) {
         cookies.forEach(cookie -> {
             cookie.setPath("/");
             response.addCookie(cookie);
