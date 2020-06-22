68086868d1aa51c14bc32e402e0405e66ee3f328
Kim HyunSik <hsik0225@gmail.com>
2020-06-22 16:21:26 +0900

    [37] feat: GradleDaemon 종료 기능 추가

--- BE/deploy-be.sh
+++ BE/deploy-be.sh
@@ -8,6 +8,7 @@ JAR_NAME=$(basename $REPOSITORY/build/libs/*.jar)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
+CURRENT_GRADLE_PID=$(pgrep -f GradleDaemon)
 
 if [ -z $CURRENT_PID ]
 then
@@ -20,3 +21,12 @@ fi
 
 echo "> $JAR_PATH 배포"
 nohup java -jar -Dspring.profiles.active=production -Djasypt.encryptor.password=everjay $JAR_PATH &
+
+if [ -z $CURRENT_GRADLE_PID ]
+then
+	echo "> 종료할 것 없음"
+else
+	echo "kill $CURRENT_GRADLE_PID"
+	kill -9 $CURRENT_GRADLE_PID
+	sleep 5
+fi
