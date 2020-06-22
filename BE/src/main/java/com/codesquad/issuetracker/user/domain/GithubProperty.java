9e8a0e4f973d04c1b2ea2d91cc2ac8dc692bbdbb
Kim HyunSik <hsik0225@gmail.com>
2020-06-22 13:20:44 +0900

    [#70] refactor: redirect 경로를 배포 경로로 변경

--- BE/src/main/java/com/codesquad/issuetracker/user/domain/GithubProperty.java
+++ BE/src/main/java/com/codesquad/issuetracker/user/domain/GithubProperty.java
@@ -5,11 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
 
 @Getter
 @Setter
