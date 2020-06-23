b3c72067339f3c4a0bc4d20442aaffd79cb7806e
Lee SeEun <sally4405@naver.com>
2020-06-24 01:53:23 +0900

    [#72] fix: 마일스톤 변경 api parameter 객체 형태로 변경

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,9 +4,9 @@ import { API_URL } from "@Constants/url";
 export const getIssue = () => axios.get(API_URL.issue);
 
 export const getMilestone = () => axios.get(API_URL.milestone);
-export const getMilestoneDetail = (milestoneId) => axios.get(`${API_URL.milestone}${milestoneId}/issues`);
+export const getMilestoneDetail = (milestoneId) => axios.get(`${API_URL.milestone}${milestoneId}`);
 export const postMilestone = (params) => axios.post(`${API_URL.milestone}`, params);
-export const putMilestone = (milestoneId, params) => axios.put(`${API_URL.milestone}${milestoneId}`, params);
+export const putMilestone = ({ milestoneId, params }) => axios.put(`${API_URL.milestone}${milestoneId}`, params);
 export const patchMilestone = (milestoneId) => axios.patch(`${API_URL.milestone}${milestoneId}`);
 export const deleteMilestone = (milestoneId) => axios.delete(`${API_URL.milestone}${milestoneId}`);
 
