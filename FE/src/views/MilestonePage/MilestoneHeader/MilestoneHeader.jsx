97e6138065e7c19fc65aa3db74fa4cbb6d1a658c
Lee SeEun <sally4405@naver.com>
2020-06-24 01:55:48 +0900

    [#72] feat: 마일스톤 목록 페이지에서 table header의 open, close에 스타일 추가

--- FE/src/views/MilestonePage/MilestoneHeader/MilestoneHeader.jsx
+++ FE/src/views/MilestonePage/MilestoneHeader/MilestoneHeader.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import EventNoteIcon from "@material-ui/icons/EventNote";
 import DoneIcon from "@material-ui/icons/Done";
@@ -6,14 +6,28 @@ import DoneIcon from "@material-ui/icons/Done";
 import Text from "@Style/Text";
 
 const MilestoneHeader = ({ openCount, closeCount, open, close }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const [isClose, setIsClose] = useState(false);
+
+  const onOpen = () => {
+    open();
+    setIsOpen(true);
+    setIsClose(false);
+  };
+  const onClose = () => {
+    close();
+    setIsOpen(false);
+    setIsClose(true);
+  };
+
   return (
     <>
       <Wrapper>
-        <HeaderLink fontWeight="bold" as="a" onClick={open}>
+        <HeaderLink as="a" onClick={onOpen} isOpen={isOpen}>
           <EventNoteIcon fontSize="small" />
           {openCount} Open
         </HeaderLink>
-        <HeaderLink fontWeight="bold" as="a" onClick={close}>
+        <HeaderLink as="a" onClick={onClose} isOpen={isClose}>
           <DoneIcon fontSize="small" />
           {closeCount} Close
         </HeaderLink>
@@ -35,6 +49,11 @@ const HeaderLink = styled(Text)`
   cursor: pointer;
   svg {
     margin-right: 5px;
+  }
+  color: ${(props) => (props.isOpen ? props.theme.colors.black : props.theme.colors.gray3)};
+  font-weight: ${(props) => (props.isOpen ? props.theme.fontWeights.bold : "")};
+  &:hover {
+    color: ${(props) => (props.isOpen ? "" : props.theme.colors.black)};
   }
 `;
 
