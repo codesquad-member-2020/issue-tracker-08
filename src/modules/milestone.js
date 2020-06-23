aaee117e6fdeced9d5355b089d99145762b603fb
Lee SeEun <sally4405@naver.com>
2020-06-24 01:52:19 +0900

    [#72] fix: 마일스톤 모듈 불필요한 액션 제거, 파라미터 제거

--- src/modules/milestone.js
+++ src/modules/milestone.js
@@ -9,23 +9,16 @@ const GET_MILESTONE_DETAIL = "milestone/GET_MILESTONE_DETAIL";
 const GET_MILESTONE_DETAIL_SUCCESS = "milestone/GET_MILESTONE_DETAIL_SUCCESS";
 
 const POST_MILESTONE = "milestone/POST_MILESTONE";
-const POST_MILESTONE_SUCCESS = "milestone/POST_MILESTONE_SUCCESS";
-
 const PUT_MILESTONE = "milestone/PUT_MILESTONE";
-const PUT_MILESTONE_SUCCESS = "milestone/PUT_MILESTONE_SUCCESS";
-
 const PATCH_MILESTONE = "milestone/PATCH_MILESTONE";
-const PATCH_MILESTONE_SUCCESS = "milestone/PATCH_MILESTONE_SUCCESS";
-
 const DELETE_MILESTONE = "milestone/DELETE_MILESTONE";
-const DELETE_MILESTONE_SUCCESS = "milestone/DELETE_MILESTONE_SUCCESS";
 
 export const getMilestone = createRequestThunk(GET_MILESTONE, api.getMilestone);
-export const getMilestoneDetail = (milestoneId) => createRequestThunk(GET_MILESTONE_DETAIL, api.getMilestoneDetail(milestoneId));
-export const postMilestone = (params) => createRequestThunk(POST_MILESTONE, api.postMilestone(params));
-export const putMilestone = (milestoneId, params) => createRequestThunk(PUT_MILESTONE, api.putMilestone(milestoneId, params));
-export const patchMilestone = (milestoneId) => createRequestThunk(PATCH_MILESTONE, api.patchMilestone(milestoneId));
-export const deleteMilestone = (milestoneId) => createRequestThunk(DELETE_MILESTONE, api.deleteMilestone(milestoneId));
+export const getMilestoneDetail = createRequestThunk(GET_MILESTONE_DETAIL, api.getMilestoneDetail);
+export const postMilestone = createRequestThunk(POST_MILESTONE, api.postMilestone);
+export const putMilestone = createRequestThunk(PUT_MILESTONE, api.putMilestone);
+export const patchMilestone = createRequestThunk(PATCH_MILESTONE, api.patchMilestone);
+export const deleteMilestone = createRequestThunk(DELETE_MILESTONE, api.deleteMilestone);
 
 const initialState = {
   milestones: null,
@@ -41,10 +34,6 @@ const milestone = handleActions(
     [GET_MILESTONE_DETAIL_SUCCESS]: (state, action) => ({
       ...state,
       milestoneDetail: action.payload,
-    }),
-    [DELETE_MILESTONE_SUCCESS]: (state, action) => ({
-      ...state,
-      milestones: state.milestones.filter((milestone) => milestone !== action.payload),
     }),
   },
   initialState
