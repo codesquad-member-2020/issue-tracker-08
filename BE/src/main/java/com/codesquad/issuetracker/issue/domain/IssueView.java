88d47bdb69a9205875a25d32de510dee6195504f
Kim HyunSik <hsik0225@gmail.com>
2020-06-23 15:11:43 +0900

    [#55] feat: IssueView, CommentView @Getter, @Setter, @생성자 추가

--- BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueView.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueView.java
@@ -5,9 +5,17 @@ import com.codesquad.issuetracker.label.domain.Label;
 import com.codesquad.issuetracker.milestone.domain.Milestone;
 import com.codesquad.issuetracker.user.domain.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
 public class IssueView {
 
     @JsonIgnoreProperties({"authorId", "assignees", "milestoneId", "labels"})
