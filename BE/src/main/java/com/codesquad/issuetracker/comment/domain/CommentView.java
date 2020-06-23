19e6cc48dd074d2a703a0d000a42859ce0f1176c
Kim HyunSik <hsik0225@gmail.com>
2020-06-23 12:05:46 +0900

    [#55] feat: Comment를 화면에 보여주는 DTO 생성

---
+++ BE/src/main/java/com/codesquad/issuetracker/comment/domain/CommentView.java
@@ -0,0 +1,10 @@
+package com.codesquad.issuetracker.comment.domain;
+
+import com.codesquad.issuetracker.user.domain.User;
+
+public class CommentView {
+
+    private Comment comment;
+
+    private User user;
+}
