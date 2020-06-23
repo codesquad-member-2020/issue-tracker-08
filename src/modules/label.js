b4afce76a3878660553261d5a06bf391e944db22
choisohyun <sohyun4283@naver.com>
2020-06-23 23:18:21 +0900

    [#65] style: 사용하지 않는 액션 삭제
    - GET_LABEL_SUCCESS를 제외한 액션은 필요하지 않음
      - 필요하지 않은 이유: 실시간 업데이트에 맞춰 동시에 여러 번 수정해도
        현재 데이터를 보여줄 수 있게 하기 위해 다른 요청이 성공하면 GET을 다시 해 오게끔 해야 함

--- src/modules/label.js
+++ src/modules/label.js
@@ -4,17 +4,9 @@ import * as api from "@Lib/api";
 
 const GET_LABEL = "label/GET_LABEL";
 const GET_LABEL_SUCCESS = "label/GET_LABEL_SUCCESS";
-
 const POST_LABEL = "label/POST_LABEL";
-const POST_LABEL_SUCCESS = "label/POST_LABEL_SUCCESS";
-
 const EDIT_LABEL = "label/EDIT_LABEL";
-const EDIT_LABEL_SUCCESS = "label/EDIT_LABEL_SUCCESS";
-const EDIT_LABEL_FAILURE = "label/EDIT_LABEL_FAILURE";
-
 const DELETE_LABEL = "label/DELETE_LABEL";
-const DELETE_LABEL_SUCCESS = "label/DELETE_LABEL_SUCCESS";
-const DELETE_LABEL_FAILURE = "label/DELETE_LABEL_FAILURE";
 
 export const getLabel = createRequestThunk(GET_LABEL, api.getLabel);
 export const createLabel = (params) => createRequestThunk(POST_LABEL, api.createLabel(params));
@@ -28,22 +20,6 @@ const initialState = {
 const label = handleActions(
   {
     [GET_LABEL_SUCCESS]: (state, action) => ({
-      ...state,
-      labels: action.payload,
-    }),
-    [POST_LABEL_SUCCESS]: (state, action) => ({
-      ...state,
-      labels: state.concat(action.payload),
-    }),
-    [DELETE_LABEL_SUCCESS]: (state, action) => ({
-      ...state,
-      labels: state.labels.filter((label) => label !== action.payload),
-    }),
-    [EDIT_LABEL_SUCCESS]: (state, action) => ({
-      ...state,
-      labels: action.payload,
-    }),
-    [DELETE_LABEL_FAILURE]: (state, action) => ({
       ...state,
       labels: action.payload,
     }),
