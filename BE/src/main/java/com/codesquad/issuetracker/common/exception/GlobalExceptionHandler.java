dc9029a344efcc96c3aa80cf2229675b21890cbc
Kim HyunSik <hsik0225@gmail.com>
2020-06-24 18:52:16 +0900

    [#58] feat: EntityNotFoundException ControllerAdvice에 추가

--- BE/src/main/java/com/codesquad/issuetracker/common/exception/GlobalExceptionHandler.java
+++ BE/src/main/java/com/codesquad/issuetracker/common/exception/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.persistence.EntityNotFoundException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -17,5 +19,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<String> handleDuplicateEntryException(DataIntegrityViolationException e) {
         return new ResponseEntity<>(ErrorMessage.MILESTONE_TITLE_DUPLICATED.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
+        return new ResponseEntity<>(ErrorMessage.ENTITY_NOT_FOUND.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 }
