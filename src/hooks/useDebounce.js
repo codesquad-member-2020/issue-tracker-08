0fcbed60a11c1cc78ae69e34ba2ad230bb466ffa
choisohyun <sohyun4283@naver.com>
2020-06-24 14:52:47 +0900

    [#84] feat: default Delay를 500ms로 하는 Debounce 커스텀훅 생성

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+const useDebounce = (value, delay = 500) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timer);
+  }, [value]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
