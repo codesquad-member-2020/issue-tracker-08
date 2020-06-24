af3c951d534c70e99fd35cc6bb5f846be8ab718c
Jay <beginin15@gmail.com>
2020-06-24 20:10:09 +0900

    [#55] fix: dev 리베이스 충돌 해결

--- BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueRepository.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueRepository.java
@@ -1,11 +1,12 @@
 package com.codesquad.issuetracker.issue.domain;
 
 import com.codesquad.issuetracker.milestone.domain.MilestoneId;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface IssueRepository extends CrudRepository<Issue, IssueId> {
+public interface IssueRepository extends CrudRepository<Issue, IssueId>, QuerydslPredicateExecutor<Issue> {
 
     Issue findFirstByOrderByIdDesc();
 
