a1094965985aa8853da0f34356a25bc1d7d8d38f
Jay <beginin15@gmail.com>
2020-06-24 15:45:57 +0900

    [#58] fix: dev 리베이스 충돌 해결

--- BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueRepository.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueRepository.java
@@ -1,7 +1,18 @@
 package com.codesquad.issuetracker.issue.domain;
 
+import com.codesquad.issuetracker.milestone.domain.MilestoneId;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IssueRepository extends CrudRepository<Issue, IssueId> {
+
     Issue findFirstByOrderByIdDesc();
+
+    @Query("SELECT i " +
+            "FROM  Issue AS i " +
+            "WHERE milestone_id = :#{#milestoneId.milestoneId}")
+    List<Issue> findByMilestoneId(@Param(value = "milestoneId") MilestoneId milestoneId);
 }
