2987b44e1575e1f8188716922ac5d65c2c2bd9a5
choisohyun <sohyun4283@naver.com>
2020-06-24 21:33:59 +0900

    [#84] fix: 초기값만 있을 때에는 에러메시지가 나오지 않도록 early return 처리

--- src/views/SignupPage/SignupPage.jsx
+++ src/views/SignupPage/SignupPage.jsx
@@ -28,11 +28,13 @@ const SignupPage = ({ isSignupOpen, openHandler }) => {
   }, [debounceUserInfo, debouncedpwdCheck]);
 
   const validId = () => {
+    if (!userInfo.id) return;
     if (!userInfo.id.match(REGEX.ID)) return setIdMsg(ID_MSG.INVALID);
     return setIdMsg(ID_MSG.SUCCESS);
   };
 
   const validPassword = () => {
+    if (!userInfo.password) return;
     if (!userInfo.password.match(REGEX.PSWD_LEN)) return setPasswordMsg(PSWD1_MSG.INVALID_LEN);
     if (!userInfo.password.match(REGEX.DOWN_CASE)) return setPasswordMsg(PSWD1_INVALID_CASE.ENG_DOWN + PSWD1_MSG.INVALID);
     if (!userInfo.password.match(REGEX.NUMBER)) return setPasswordMsg(PSWD1_INVALID_CASE.NUMBER + PSWD1_MSG.INVALID);
@@ -41,11 +43,13 @@ const SignupPage = ({ isSignupOpen, openHandler }) => {
   };
 
   const validPasswordCheck = () => {
+    if (!passwordCheck) return;
     if (userInfo.password !== passwordCheck) return setPasswordCheckMsg(PSWD2_MSG.FAIL);
     return setPasswordCheckMsg(PSWD2_MSG.SUCCESS);
   };
 
   const validEmail = () => {
+    if (!userInfo.email) return;
     if (!userInfo.email.match(REGEX.EMAIL)) return setEmailMsg(EMAIL_ERR_MSG);
   };
 
