e48447905601bb602c781db1941a22a14a709b86
Kim HyunSik <hsik0225@gmail.com>
2020-06-24 18:26:09 +0900

    [#55] refactor: @Query를 Criteria로 수

--- BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueRepository.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/domain/IssueRepository.java
@@ -1,9 +1,7 @@
 package com.codesquad.issuetracker.issue.domain;
 
 import com.codesquad.issuetracker.milestone.domain.MilestoneId;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,8 +9,5 @@ public interface IssueRepository extends CrudRepository<Issue, IssueId> {
 
     Issue findFirstByOrderByIdDesc();
 
-    @Query("SELECT i " +
-            "FROM  Issue AS i " +
-            "WHERE milestone_id = :#{#milestoneId.milestoneId}")
-    List<Issue> findByMilestoneId(@Param(value = "milestoneId") MilestoneId milestoneId);
+    List<Issue> findAllByMilestoneId(MilestoneId milestoneId);
 }
