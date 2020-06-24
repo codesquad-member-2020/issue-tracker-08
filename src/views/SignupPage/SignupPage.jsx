418deef7feaf2a4732499deee50718ebadfb88f1
choisohyun <sohyun4283@naver.com>
2020-06-24 14:56:20 +0900

    [#84] refactor: 회원가입을 별도의 컴포넌트로 분리
    - 유효성 검사를 하면서 컴포넌트가 커질 것을 대비해 분리함

--- src/views/SignupPage/SignupPage.jsx
+++ src/views/SignupPage/SignupPage.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import styled from "styled-components";
 
-const SignupPage = () => {
-  return <div></div>;
+import Button from "@Style/Button";
+
+import PersonalInputBox from "@InputBox/PersonalInputBox";
+
+const SignupPage = ({ isSignupOpen, openHandler }) => {
+  return (
+    <>
+      <SignupWrap isSignupOpen={isSignupOpen}>
+        <PersonalInputBox title="아이디" />
+        <PersonalInputBox title="비밀번호" type="password" />
+        <PersonalInputBox title="비밀번호 확인" type="password" />
+        <PersonalInputBox title="이메일" />
+        <SignUpButtonWrap>
+          <Button backgroundColor="blue" style={loginButtonStyle} onClick={openHandler}>
+            회원가입
+          </Button>
+        </SignUpButtonWrap>
+      </SignupWrap>
+    </>
+  );
+};
+
+const SignupWrap = styled.div`
+  display: ${(props) => (props.isSignupOpen ? "block" : "none")};
+`;
+
+const SignUpButtonWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 32px;
+`;
+
+const loginButtonStyle = {
+  width: "49%",
+  textAlign: "center",
 };
 
 export default SignupPage;
