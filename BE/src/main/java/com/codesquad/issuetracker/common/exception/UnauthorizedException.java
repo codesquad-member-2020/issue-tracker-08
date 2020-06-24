69c7a96b63c3223b692afba8120b0a3257c66e94
Kim HyunSik <hsik0225@gmail.com>
2020-06-24 20:00:58 +0900

    [#100] feat: UnauthorizedException 생성자에 메세지 추가

--- BE/src/main/java/com/codesquad/issuetracker/common/exception/UnauthorizedException.java
+++ BE/src/main/java/com/codesquad/issuetracker/common/exception/UnauthorizedException.java
@@ -2,6 +2,10 @@ package com.codesquad.issuetracker.common.exception;
 
 public class UnauthorizedException extends RuntimeException {
 
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
     public UnauthorizedException() {
         super(ErrorMessage.UNAUTHORIZED.getMessage());
     }
