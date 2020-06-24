b267c1e1e67dc74ddfe8928497d78c15ba18a8c4
Kim HyunSik <hsik0225@gmail.com>
2020-06-24 19:11:24 +0900

    [#100] feat: ErrorMessage 추가

--- BE/src/main/java/com/codesquad/issuetracker/common/exception/ErrorMessage.java
+++ BE/src/main/java/com/codesquad/issuetracker/common/exception/ErrorMessage.java
@@ -6,13 +6,23 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorMessage {
-  
+
+    // COMMON
+    INTERNAL_SERVER_ERROR("Backend Error"),
+    METHOD_NOT_ALLOWED("Method Not Allowed"),
+
+    // ENTITY
     LABEL_NAME_DUPLICATED("Name has already been taken"),
     MILESTONE_TITLE_DUPLICATED("Title has already been taken"),
     ENTITY_UPDATE_FAILED("Failed to update"),
     ENTITY_DELETE_FAILED("Failed to delete"),
+    ENTITY_NOT_FOUND("Failed to get"),
+
+    // USER
     UNAUTHORIZED("Need to Login"),
-    ENTITY_NOT_FOUND("Failed to get");
+    ACCESS_DENIED("Access is Denied"),
+    INVALID_JWT_TOKEN("Invalid Jwt Token"),
+    INVALID_INPUT_VALUE("Invalid Input Value");
 
     private String message;
     
