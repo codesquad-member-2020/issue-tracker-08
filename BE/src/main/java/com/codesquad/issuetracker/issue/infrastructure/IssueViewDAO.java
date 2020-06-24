ab29e5aa1bc94aa7213436f488fb67f7457eb26c
Kim HyunSik <hsik0225@gmail.com>
2020-06-24 12:57:26 +0900

    [#55] refactor: IssueViewDAO에서 DTO 생성 메소드 삭제

--- BE/src/main/java/com/codesquad/issuetracker/issue/infrastructure/IssueViewDAO.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/infrastructure/IssueViewDAO.java
@@ -2,7 +2,6 @@ package com.codesquad.issuetracker.issue.infrastructure;
 
 import com.codesquad.issuetracker.comment.domain.CommentView;
 import com.codesquad.issuetracker.issue.domain.IssueId;
-import com.codesquad.issuetracker.issue.domain.IssueView;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -15,13 +14,6 @@ public class IssueViewDAO {
 
     @PersistenceContext
     private EntityManager entityManager;
-
-    public IssueView read(IssueId issueId) {
-        String selectQuery = "SELECT new com.codesquad.issuetracker.issue.domain.IssueView(i, u1, m) FROM Issue i, User u1, Milestone m WHERE i.authorId = u1.id AND i.milestoneId = m.id AND i.id = :issueId";
-        TypedQuery<IssueView> query = entityManager.createQuery(selectQuery, IssueView.class);
-        query.setParameter("issueId", issueId);
-        return query.getSingleResult();
-    }
 
     public List<CommentView> readAllComment(IssueId issueId) {
         String selectQuery = "SELECT new com.codesquad.issuetracker.comment.domain.CommentView(c, u1) FROM Comment c, User u1 WHERE c.id.issueId = :issueId AND c.id.userId = u1.id.userId";
