7b1342e036c86adfa5b90ad1011c21f88df27d4e
Jay <beginin15@gmail.com>
2020-06-24 20:02:19 +0900

    [#55] refactor: QueryDsl의 Predicate 객체를 이용하여 레포지토리에서 조회하도록 변경

--- BE/src/main/java/com/codesquad/issuetracker/issue/application/IssueService.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/application/IssueService.java
@@ -1,10 +1,9 @@
 package com.codesquad.issuetracker.issue.application;
 
+import com.codesquad.issuetracker.comment.domain.CommentRepository;
 import com.codesquad.issuetracker.comment.domain.CommentView;
-import com.codesquad.issuetracker.issue.domain.Issue;
-import com.codesquad.issuetracker.issue.domain.IssueId;
-import com.codesquad.issuetracker.issue.domain.IssueRepository;
-import com.codesquad.issuetracker.issue.domain.IssueView;
+import com.codesquad.issuetracker.issue.domain.*;
+import com.codesquad.issuetracker.issue.infrastructure.IssuePredicate;
 import com.codesquad.issuetracker.issue.infrastructure.IssueViewDAO;
 import com.codesquad.issuetracker.label.domain.Label;
 import com.codesquad.issuetracker.label.domain.LabelId;
@@ -15,11 +14,6 @@ import com.codesquad.issuetracker.milestone.domain.MilestoneId;
 import com.codesquad.issuetracker.user.domain.User;
 import com.codesquad.issuetracker.user.domain.UserId;
 import com.codesquad.issuetracker.user.domain.UserRepository;
-import com.codesquad.issuetracker.comment.application.CommentService;
-import com.codesquad.issuetracker.issue.domain.*;
-import com.codesquad.issuetracker.label.application.LabelService;
-import com.codesquad.issuetracker.milestone.application.MilestoneService;
-import com.codesquad.issuetracker.user.application.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -27,8 +21,8 @@ import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 @RequiredArgsConstructor
@@ -37,42 +31,33 @@ public class IssueService {
     private final IssueViewDAO issueViewDAO;
 
     private final IssueRepository issueRepository;
-    private final LabelService labelService;
-    private final MilestoneService milestoneService;
-    private final UserService userService;
-    private final CommentService commentService;
+
+    private final UserRepository userRepository;
+
+    private final CommentRepository commentRepository;
+
+    private final LabelRepository labelRepository;
+
+    private final MileStoneRepository mileStoneRepository;
 
     public IssueBoard findIssuesByFilter(Filter filter) {
-
-        List<IssueResponse> issues = issueRepository.findByFilter(filter).stream()
-                .map(i -> IssueResponse.builder()
-                        .id(i.getId().getIssueId())
-                        .title(i.getTitle())
-                        .content(i.getContent())
-                        .isOpen(i.getIsOpen())
-                        .createdAt(i.getCreatedAt())
-                        .author(userService.findById(i.getAuthorId()))
-                        .assignees(userService.findAllByIds(i.getAssignees()))
-                        .labels(labelService.findAllByIds(i.getLabels()))
-                        .numberOfComment(commentService.countByIssueId(i.getId()))
-                        .milestone(milestoneService.readMilestoneById(i.getMilestoneId()))
-                        .build())
+        List<IssueView> issues = StreamSupport.stream(issueRepository.findAll(IssuePredicate.search(filter)).spliterator(), false)
+                .map(i -> IssueView.of(i,
+                        userRepository.findById(i.getAuthorId()).orElseThrow(EntityNotFoundException::new),
+                        mileStoneRepository.findById(i.getMilestoneId()).orElseThrow(EntityNotFoundException::new),
+                        StreamSupport.stream(userRepository.findAllById(i.getAssignees()).spliterator(), false).collect(Collectors.toList()),
+                        StreamSupport.stream(labelRepository.findAllById(i.getLabels()).spliterator(), false).collect(Collectors.toList()),
+                        commentRepository.countByIssueId(i.getId())))
                 .collect(Collectors.toList());
 
          return IssueBoard.builder()
                 .issues(issues)
-                .numberOfLabels(labelService.count())
-                .numberOfMilestones(milestoneService.count())
-                .numberOfOpenIssues(issueRepository.countByIsOpenTrue())
-                .numberOfClosedIssues(issueRepository.countByIsOpenFalse())
+                .numberOfLabels(labelRepository.count())
+                .numberOfMilestones(mileStoneRepository.count())
+                .numberOfOpenIssue(issueRepository.countByIsOpenTrue())
+                .numberOfClosedIssue(issueRepository.countByIsOpenFalse())
                 .build();
     }
-
-    private final LabelRepository labelRepository;
-
-    private final UserRepository userRepository;
-
-    private final MileStoneRepository mileStoneRepository;
 
     public Issue createIssue(Issue issue) {
         Issue newIssue = Issue.of(getNextIdentity(), issue);
