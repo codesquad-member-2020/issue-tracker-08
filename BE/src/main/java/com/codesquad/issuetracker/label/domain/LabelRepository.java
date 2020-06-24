3f67941efbcad81f5d58b79ec7d3a00c4012e180
Kim HyunSik <hsik0225@gmail.com>
2020-06-24 11:09:17 +0900

    [#55] refactor: labelRepository에서 IssueId로 라벨 목록을 가져오는 메소드 삭제
    - IssueId로 조인해서 찾지 않고 Issue가 가지고 있는 LabelId로 Label을 찾으면 된다고 생각하여 삭제

--- BE/src/main/java/com/codesquad/issuetracker/label/domain/LabelRepository.java
+++ BE/src/main/java/com/codesquad/issuetracker/label/domain/LabelRepository.java
@@ -1,15 +1,8 @@
 package com.codesquad.issuetracker.label.domain;
 
-import com.codesquad.issuetracker.issue.domain.IssueId;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface LabelRepository extends CrudRepository<Label, LabelId> {
 
-    public Label findFirstByOrderByIdDesc();
-
-    @Query(value = "SELECT l.label_id, l.color, l.description, l.is_font_color_black, l.name FROM Label l JOIN tag t ON l.label_id = t.label_id JOIN issue i ON t.issue_id = i.issue_id WHERE i.issue_id= :issueId", nativeQuery = true)
-    List<Label> findAllByIssueId(IssueId issueId);
+    Label findFirstByOrderByIdDesc();
 }
