e3b7eb2ef8bc9ce0887d7a97761e21e20785e014
Kim HyunSik <hsik0225@gmail.com>
2020-06-24 18:22:06 +0900

    [#58] feat: ControllerAdvice에 unique 키 중복 입력 에러 추가

--- BE/src/main/java/com/codesquad/issuetracker/common/exception/GlobalExceptionHandler.java
+++ BE/src/main/java/com/codesquad/issuetracker/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.codesquad.issuetracker.common.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,5 +12,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDuplicateEntryException(DataIntegrityViolationException e) {
+        return new ResponseEntity<>(ErrorMessage.MILESTONE_TITLE_DUPLICATED.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 }
