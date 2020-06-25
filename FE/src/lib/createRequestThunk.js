4c6a318dacaace44a196e9d4df537785b4f9cac0
choisohyun <sohyun4283@naver.com>
2020-06-25 15:52:53 +0900

    [#84] fix: payload를 response로 보내도록 변경
    - status를 회원가입/로그인에서 사용하기 위해 변경함

--- FE/src/lib/createRequestThunk.js
+++ FE/src/lib/createRequestThunk.js
@@ -8,19 +8,20 @@ const createRequestThunk = (type, request) => {
     dispatch(startLoading(type));
     try {
       const response = await request(params);
+      console.log(response);
       dispatch({
         type: SUCCESS,
-        payload: response.data,
+        payload: response,
       });
       dispatch(finishLoading(type));
     } catch (e) {
+      console.log(e);
       dispatch({
         type: FAILURE,
         payload: e,
         error: true,
       });
       dispatch(startLoading(type));
-      throw e;
     }
   };
 };
