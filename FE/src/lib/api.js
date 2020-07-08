a964d9896dc00c1867ad3f6502ac8a51cdb094a2
sally4405 <sally4405@naver.com>
2020-07-09 04:29:39 +0900

    [#118] feat: 코멘트 삭제 api 추가

--- FE/src/lib/api.js
+++ FE/src/lib/api.js
@@ -4,6 +4,7 @@ import { API_URL } from "@Constants/url";
 export const getIssue = () => axios.get(API_URL.issue);
 export const getDetailIssue = (issueId) => axios.get(`${API_URL.issue}${issueId}`);
 export const postComment = ({ issueId, params }) => axios.post(`${API_URL.issue}${issueId}/comments`, params);
+export const deleteComment = ({ issueId, commentId }) => axios.delete(`${API_URL.issue}${issueId}/comments/${commentId}`);
 
 export const getMilestone = () => axios.get(API_URL.milestone);
 export const getMilestoneDetail = (milestoneId) => axios.get(`${API_URL.milestone}${milestoneId}`);
