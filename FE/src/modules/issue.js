3e30455c1a6d9cc46012273ffb3d9b5bbd8629c1
sally4405 <sally4405@naver.com>
2020-07-09 04:22:40 +0900

    [#117] feat: 이슈 모듈에 코멘트 생성 함수 추가

--- FE/src/modules/issue.js
+++ FE/src/modules/issue.js
@@ -8,8 +8,10 @@ const GET_ISSUE_SUCCESS = "issue/GET_ISSUE_SUCCESS";
 const GET_DETAIL_ISSUE = "issue/GET_DETAIL_ISSUE";
 const GET_DETAIL_ISSUE_SUCCESS = "issue/GET_DETAIL_ISSUE_SUCCESS";
 
+const POST_COMMENT = "issue/POST_COMMENT";
 export const getIssue = createRequestThunk(GET_ISSUE, api.getIssue);
 export const getDetailIssue = createRequestThunk(GET_DETAIL_ISSUE, api.getDetailIssue);
+export const postComment = createRequestThunk(POST_COMMENT, api.postComment);
 
 const initialState = {
   issues: null,
