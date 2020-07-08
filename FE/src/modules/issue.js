ed23b4db90b38bcc70511b487b5f7250a9cf11b8
choisohyun <sohyun4283@naver.com>
2020-07-08 16:13:01 +0900

    [#125] feat: 기존 이슈 모듈에 이슈 상세 GET 요청 액션 추가

--- FE/src/modules/issue.js
+++ FE/src/modules/issue.js
@@ -5,10 +5,15 @@ import * as api from "@Lib/api";
 const GET_ISSUE = "issue/GET_ISSUE";
 const GET_ISSUE_SUCCESS = "issue/GET_ISSUE_SUCCESS";
 
+const GET_DETAIL_ISSUE = "issue/GET_DETAIL_ISSUE";
+const GET_DETAIL_ISSUE_SUCCESS = "issue/GET_DETAIL_ISSUE_SUCCESS";
+
 export const getIssue = createRequestThunk(GET_ISSUE, api.getIssue);
+export const getDetailIssue = createRequestThunk(GET_DETAIL_ISSUE, api.getDetailIssue);
 
 const initialState = {
   issues: null,
+  detailIssue: null,
 };
 
 const issue = handleActions(
@@ -16,6 +21,10 @@ const issue = handleActions(
     [GET_ISSUE_SUCCESS]: (state, action) => ({
       ...state,
       issues: action.payload.data.issues,
+    }),
+    [GET_DETAIL_ISSUE_SUCCESS]: (state, action) => ({
+      ...state,
+      detailIssue: action.payload.data,
     }),
   },
   initialState
