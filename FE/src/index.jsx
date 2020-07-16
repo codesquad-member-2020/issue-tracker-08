162cceeb1e231c264606e6e2ed06d813ab4a2683
choisohyun <sohyun4283@naver.com>
2020-07-16 18:11:51 +0900

    feat: redux-devtools-extension 적용

--- FE/src/index.jsx
+++ FE/src/index.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import ReduxThunk from "redux-thunk";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import App from "@/App";
 import modules from "@/modules";
 
 const rootElement = document.getElementById("root");
-const store = createStore(modules, applyMiddleware(ReduxThunk));
+const middleware = applyMiddleware(ReduxThunk);
+
+const store = createStore(modules, composeWithDevTools(middleware));
 
 ReactDOM.render(
   <Provider store={store}>
