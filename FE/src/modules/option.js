8d9aa629d81e2397e37f83a1f8d0682192b592d1
choisohyun <sohyun4283@naver.com>
2020-07-17 15:23:56 +0900

    [#144] feat: createRequestThunk 실행하는 부분 액션에 추가

--- FE/src/modules/option.js
+++ FE/src/modules/option.js
@@ -1,12 +1,17 @@
+import createRequestThunk from "@Lib/createRequestThunk";
+import * as api from "@Lib/api";
+
 const SAVE_OPTION = "option/SAVE_OPTION";
+const RESET_OPTION = "option/RESET_OPTION";
+const SAVE_ASSIGNEES = "option/SAVE_ASSIGNEES";
 const SAVE_LABELS = "option/SAVE_LABELS";
 const SAVE_MILESTONE = "option/SAVE_MILESTONE";
-const RESET_OPTION = "option/RESET_OPTION";
 
 export const saveOption = (data, title) => ({ type: SAVE_OPTION, data, title });
-export const saveLabels = (data) => ({ type: SAVE_LABELS, data });
-export const saveMilestone = (data) => ({ type: SAVE_MILESTONE, data });
 export const resetOption = () => ({ type: RESET_OPTION });
+export const saveAssignees = createRequestThunk(SAVE_ASSIGNEES, api.changeAssignee);
+export const saveLabels = createRequestThunk(SAVE_LABELS, api.changeLabels);
+export const saveMilestone = createRequestThunk(SAVE_MILESTONE, api.changeMilestone);
 
 const initialState = {
   author: null,
