a3fababd8818daf65d61939bf39a13ff4beaa4fe
Jay <beginin15@gmail.com>
2020-07-17 16:47:04 +0900

    [#145] feat: 이슈 목록 요청 시, Query parameter로 페이지 수 받도록 필드 추가
    - setter를 정의하여 파라미터로 전달받는 페이지는 1부터 시작하고 실제 페이지를 처리할 때는 0부터 시작하도록 처리
    
    - getPageRequest(): 이슈 레포지토리의 findAll 메소드에 전달할 PageRequest를 만들어 반환하는 메소드 정의

--- BE/src/main/java/com/codesquad/issuetracker/issue/domain/Filter.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/domain/Filter.java
@@ -4,12 +4,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.domain.PageRequest;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
 public class Filter {
+
+    private static final int PAGE_SIZE = 5;
 
     private Boolean isOpen = true;
 
@@ -22,4 +25,14 @@ public class Filter {
     private Long assignee;
 
     private SortBy sort;
+
+    private int page;
+
+    public void setPage(int page) {
+        this.page = page <= 0 ? 0 : page - 1;
+    }
+
+    public PageRequest getPageRequest() {
+        return PageRequest.of(page, PAGE_SIZE);
+    }
 }
