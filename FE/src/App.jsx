a4f18530c947b652f9b61b945ac1335e597e8cb7
choisohyun <sohyun4283@naver.com>
2020-07-22 20:08:51 +0900

    [#156] feat: react의 Suspense와 lazy로 라우터에서 code split 처리

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyle from "@Style/GlobalStyle";
 import theme from "@Style/theme";
 
-import LoginPage from "@LoginPage/LoginPage";
-import IssueListPage from "@IssueListPage/IssueListPage";
-import CreateIssuePage from "@CreateIssuePage/CreateIssuePage";
-import IssueDetailPage from "@IssueDetailPage/IssueDetailPage";
-import LabelListPage from "@LabelListPage/LabelListPage";
-import MilestonePage from "@MilestonePage/MilestonePage";
-import CreateMilestonePage from "@CreateMilestonePage/CreateMilestonePage";
-import ErrorPage from "@ErrorPage/ErrorPage";
+const LoginPage = lazy(() => import("@LoginPage/LoginPage"));
+const IssueListPage = lazy(() => import("@IssueListPage/IssueListPage"));
+const CreateIssuePage = lazy(() => import("@CreateIssuePage/CreateIssuePage"));
+const IssueDetailPage = lazy(() => import("@IssueDetailPage/IssueDetailPage"));
+const LabelListPage = lazy(() => import("@LabelListPage/LabelListPage"));
+const MilestonePage = lazy(() => import("@MilestonePage/MilestonePage"));
+const CreateMilestonePage = lazy(() => import("@CreateMilestonePage/CreateMilestonePage"));
+const ErrorPage = lazy(() => import("@ErrorPage/ErrorPage"));
 
 const App = () => {
   return (
@@ -20,17 +20,19 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Router>
-          <Switch>
-            <Route path="/" exact component={LoginPage} />
-            <Route path="/IssueListPage" exact component={IssueListPage} />
-            <Route path="/CreateIssuePage" exact component={CreateIssuePage} />
-            <Route path="/IssueDetailPage/:issueId" exact component={IssueDetailPage} />
-            <Route path="/LabelListPage" exact component={LabelListPage} />
-            <Route path="/MilestonePage" exact component={MilestonePage} />
-            <Route path="/CreateMilestonePage/" exact component={CreateMilestonePage} />
-            <Route path="/CreateMilestonePage/:milestoneId" component={CreateMilestonePage} />
-            <Route component={ErrorPage} />
-          </Switch>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/" exact component={LoginPage} />
+              <Route path="/IssueListPage" exact component={IssueListPage} />
+              <Route path="/CreateIssuePage" exact component={CreateIssuePage} />
+              <Route path="/IssueDetailPage/:issueId" exact component={IssueDetailPage} />
+              <Route path="/LabelListPage" exact component={LabelListPage} />
+              <Route path="/MilestonePage" exact component={MilestonePage} />
+              <Route path="/CreateMilestonePage/" exact component={CreateMilestonePage} />
+              <Route path="/CreateMilestonePage/:milestoneId" component={CreateMilestonePage} />
+              <Route component={ErrorPage} />
+            </Switch>
+          </Suspense>
         </Router>
       </ThemeProvider>
     </>
