3dbe5e272a64b705a5e77a68f5543d44618cf4f8
Lee SeEun <sally4405@naver.com>
2020-07-23 12:50:16 +0900

    [#154] feat: option 모듈에 initial state로 is 추가

--- FE/src/modules/option.js
+++ FE/src/modules/option.js
@@ -3,12 +3,15 @@ import * as api from "@Lib/api";
 
 const SAVE_OPTION = "option/SAVE_OPTION";
 const RESET_OPTION = "option/RESET_OPTION";
+// const MAKE_MSG = "option/MAKE_MSG";
 const SAVE_ASSIGNEES = "option/SAVE_ASSIGNEES";
 const SAVE_LABELS = "option/SAVE_LABELS";
 const SAVE_MILESTONE = "option/SAVE_MILESTONE";
 
 export const saveOption = (data, title) => ({ type: SAVE_OPTION, data, title });
 export const resetOption = () => ({ type: RESET_OPTION });
+// export const makeMsg = (data, title) => ({ type: MAKE_MSG, data, title });
+
 export const saveAssignees = createRequestThunk(SAVE_ASSIGNEES, api.changeAssignee);
 export const saveLabels = createRequestThunk(SAVE_LABELS, api.changeLabels);
 export const saveMilestone = createRequestThunk(SAVE_MILESTONE, api.changeMilestone);
@@ -18,6 +21,8 @@ const initialState = {
   assignees: [],
   labels: [],
   milestoneId: null,
+  is: null,
+  // msg: "",
 };
 
 const titleMap = {
@@ -35,6 +40,8 @@ const option = (state = initialState, action) => {
       return { ...state, [titleMap[title]]: data };
     case RESET_OPTION:
       return { ...initialState };
+    // case MAKE_MSG:
+    //   return { ...state, msg: msg.concat(' ', `${title}:${data}`) };
     default:
       return state;
   }
