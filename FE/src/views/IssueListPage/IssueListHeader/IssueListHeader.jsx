462ebde62cd58bf413905f36ad741290f006a46d
choisohyun <sohyun4283@naver.com>
2020-07-25 17:39:27 +0900

    [#154] feat: 이슈 개폐 표시하기 위해 HeaderSwitch를 헤더에 추가
    - open과 close는 추후 추가 예정

--- FE/src/views/IssueListPage/IssueListHeader/IssueListHeader.jsx
+++ FE/src/views/IssueListPage/IssueListHeader/IssueListHeader.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 import FilterList from "@FilterButton/FilterList";
+import HeaderSwitch from "@Table/HeaderSwitch/HeaderSwitch";
 
-const IssueListHeader = ({ allCheckedHandler }) => {
+const IssueListHeader = ({ allCheckedHandler, openCount, closeCount }) => {
   const [bChecked, setChecked] = useState(false);
 
   const checkHandler = ({ target }) => {
@@ -14,6 +15,7 @@ const IssueListHeader = ({ allCheckedHandler }) => {
   return (
     <>
       <Checkbox checked={bChecked} onChange={(e) => checkHandler(e)} />
+        <HeaderSwitch openCount={openCount} closeCount={closeCount} open={open} close={close} />
       <FilterButtonWrapper>
         <FilterList isFilter bChecked={bChecked} />
       </FilterButtonWrapper>
