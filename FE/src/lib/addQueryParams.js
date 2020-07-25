6a43e5e3cae54b7c41359d1abf68dcfc47a29a81
choisohyun <sohyun4283@naver.com>
2020-07-25 18:29:10 +0900

    [#154] feat: 새로운 쿼리스트링이 기존 쿼리 값 유무에 따라 달라지도록 분기처리 추가

--- FE/src/lib/addQueryParams.js
+++ FE/src/lib/addQueryParams.js
@@ -1,12 +1,13 @@
 export const addQueryParams = (history, location, key, value) => {
   const searchParams = new URLSearchParams(location.search);
 
-  if (location.search.includes(key)) {
-    searchParams.delete(key);
-  }
+  if (location.search.includes(key)) searchParams.delete(key);
+
+  const searchString = searchParams.toString();
+  const newSearchParams = searchString === "" ? `?${key}=${value}` : `${searchString}&${key}=${value}`;
 
   history.push({
     pathname: "/IssueListPage",
-    search: `${searchParams.toString()}&${key}=${value}`,
+    search: newSearchParams,
   });
 };
