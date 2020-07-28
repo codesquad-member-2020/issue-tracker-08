b9180e17583267b75d25c17ca286d56212b0f5e0
choisohyun <sohyun4283@naver.com>
2020-07-28 09:52:51 +0900

    [#154] feat: 쿼리를 추가하는 함수 로직 변경

--- FE/src/lib/addQueryParams.js
+++ FE/src/lib/addQueryParams.js
@@ -1,10 +1,26 @@
-export const addQueryParams = (history, location, key, value) => {
+export const addQueryParams = (history, location, params) => {
   const searchParams = new URLSearchParams(location.search);
 
-  if (location.search.includes(key)) searchParams.delete(key);
+  Object.keys(params).forEach((key) => {
+    if (searchParams.has(key)) searchParams.delete(key);
+  });
 
   const searchString = searchParams.toString();
-  const newSearchParams = searchString === "" ? `?${key}=${value}` : `${searchString}&${key}=${value}`;
+  const esc = encodeURIComponent;
+  const query =
+    typeof params === "object"
+      ? Object.keys(params)
+          .map((k) => `${esc(k)}=${esc(params[k])}`)
+          .join("&")
+      : esc(params);
+  const queryParams = searchString.includes("=") ? [searchString, query].join("&") : `?${query}`;
+
+  history.push({
+    pathname: "/IssueListPage",
+    search: queryParams,
+  });
+};
+
 
   history.push({
     pathname: "/IssueListPage",
